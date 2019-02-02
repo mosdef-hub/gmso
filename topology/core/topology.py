841cc3185d0f9bfa8ea765c6a81b4717fe2e4581
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-02 14:50:44 -0600

    Add add_site function

--- topology/core/topology.py
+++ topology/core/topology.py
@@ -6,6 +6,16 @@ class Topology(object):
     name : str, optional
         A name for the Topology.
     """
-    def __init__(self, name=None):
+    def __init__(self, name=None, box=None):
         if name:
             self.name = name
+        if box:
+            self.box = box
+        self.site_list = list()
+
+    def add_site(self, site):
+        self.site_list.append(site)
+
+    @property
+    def n_sites(self):
+        return len(self.site_list)
