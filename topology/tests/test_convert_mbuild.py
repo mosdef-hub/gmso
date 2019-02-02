44054576767f17a229f1273a6eec4a4c89852138
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-02 16:48:12 -0600

    Remove debug code

--- topology/tests/test_convert_mbuild.py
+++ topology/tests/test_convert_mbuild.py
@@ -6,6 +6,5 @@ from topology.external.convert_mbuild import from_mbuild
 def test_from_mbuild():
     ethane = Ethane()
     top = from_mbuild(ethane)
-    import pdb; pdb.set_trace()
 
     assert top.n_sites == 8
