c371b1727c17596b7c991fe1fe26d97dff138f15
Alex Yang <ahy3nz@virginia.edu>
2019-02-02 21:10:27 -0600

    Add bool flag to update the site connections when constructing a connection

--- topology/core/connection.py
+++ topology/core/connection.py
@@ -1,10 +1,15 @@
 class Connection(object):
     """A simple connection between sites."""
-    def __init__(self, site1=None, site2=None):
+    def __init__(self, site1=None, site2=None, update=True):
         if site1:
             self.site1 = site1
         if site2:
             self.site2 = site2
-        site1.add_connection(site2)
-        site2.add_connection(site1)
+        if update:
+            site1.add_connection(site2)
+            site2.add_connection(site1)
 
+
+    def __eq__(self, other):
+        return ((self.site1 == other.site1 and self.site2 == other.site2) or
+                (self.site2 == other.site1 and self.site1 == other.site2))
