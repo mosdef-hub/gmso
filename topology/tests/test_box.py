361b8d9b0631c29d83135b7bfaf45e8b34fb8f7b
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-09 12:55:24 -0600

    Add test for bad box lengths

--- topology/tests/test_box.py
+++ topology/tests/test_box.py
@@ -13,6 +13,11 @@ class TestBox():
         box = Box(lengths=np.ones(3), angles=[40.0, 50.0, 60.0])
         assert np.array_equal(box.angles, [40.0, 50.0, 60.0])
 
+    @pytest.mark.parametrize('lengths', [[4.0, 4.0, 0.0], [4.0, 5.0, -1.0]])
+    def test_bad_lengths(self, lengths):
+        with pytest.raises(ValueError):
+            box = Box(lengths=lengths, angles=90*np.ones(3))
+
     def test_dtype(self):
         box = Box(lengths=np.zeros(3))
         assert box.lengths.dtype == float
