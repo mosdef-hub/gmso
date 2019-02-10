56db035a711f90f0caef0559b04568ecbab495cd
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-10 14:26:53 -0600

    Add water box to test

--- topology/tests/test_gro.py
+++ topology/tests/test_gro.py
@@ -6,7 +6,13 @@ from topology.utils.io import get_fn
 
 def test_read_gro():
     top = read_gro(get_fn('acn.gro'))
-    import pdb; pdb.set_trace()
+
     assert top.name == 'ACN'
     assert top.n_sites == 6
     assert np.allclose(top.box.lengths, 4*np.ones(3))
+
+    top = read_gro(get_fn('350-waters.gro'))
+
+    assert top.name == 'Generic title'
+    assert top.n_sites == 1050
+    assert np.allclose(top.box.lengths, 2.20866*np.ones(3))
