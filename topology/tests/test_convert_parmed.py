428eed064ad293ac6a29ad55f62b6c2430f41b19
Alex Yang <ahy3nz@virginia.edu>
2019-02-12 12:53:23 -0600

    Being tests for loading from a parametrized parmed structure

--- topology/tests/test_convert_parmed.py
+++ topology/tests/test_convert_parmed.py
@@ -4,9 +4,13 @@ from topology.external.convert_parmed import from_parmed
 from topology.utils.io import get_fn
 
 
-def test_from_parmed():
+def test_from_parmed_basic():
     struc = pmd.load_file(get_fn('ethane.mol2')).to_structure()
     top = from_parmed(struc)
 
     assert top.n_sites == 8
     assert top.n_connections == 7
+
+def test_from_parmed_parametrized_structure():
+    struc = pmd.load_file(get_fn('ethane.top'), xyz=get_fn('ethane.gro'))
+    top = from_parmed(struc)
