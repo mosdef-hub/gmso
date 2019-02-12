219fd722265c7ab545ffd9d0ad00674efe5e63e6
Alex Yang <ahy3nz@virginia.edu>
2019-02-12 12:46:37 -0600

    Begin tagging atomtype properties (with units), site coordinates (with units), box properties (wht units)

--- topology/external/convert_parmed.py
+++ topology/external/convert_parmed.py
@@ -1,9 +1,10 @@
 import parmed as pmd
-#import unyt as u
+import unyt as u
 
 from topology.core.topology import Topology
 from topology.core.site import Site
-#from topology.core.box import Box
+from topology.core.atom_type import AtomType
+from topology.core.box import Box
 
 def from_parmed(structure):
     msg = ("Provided argument that is not a Parmed Structure")
@@ -12,19 +13,34 @@ def from_parmed(structure):
     top = Topology(name=structure.title)
     map = dict()
     for atom in structure.atoms:
-        site = Site(name=atom.name, position=[atom.xx, atom.xy, atom.xz])
-        #site = Site(name=atom.name, position=[atom.xx, atom.xy, atom.xz]*u.nanometer)
+        atom_type = AtomType(name=atom.atom_type.name, 
+                charge=atom.charge * u.elementary_charge, 
+                # The parmed atomtype doens't carry the charge, but the atom does
+                # Our atomtype DOES carry the charge, but the site doens't
+                # This parallelism-contradiction might be an issue?
+                parameters={'sigma': atom.sigma * u.angstrom, 
+                    'epsilon': atom.epsilon * u.kilocalorie / u.mol})
+        site = Site(name=atom.name, position=[atom.xx, atom.xy, atom.xz]*u.angstrom)
         map[atom] = site
         top.add_site(site)
 
-    #if structure.box:
-        # top.box = Box(structure.box[0:3]*u.nanometer, angles=structure.box[4:7])
+    if structure.box:
+        # This is if we choose for topology to have abox
+        top.box = Box(structure.box[0:3]*u.angstrom, angles=structure.box[4:7])
 
     for bond in structure.bonds:
+        bond_params = {'k': bond.type.k * u.kilocalorie / (u.angstrom**2 * u.mol),
+                        'req': bond.type.req * u.angstrom}
+        # Need to modify `Connection` to have knowledge of bond parameters
         if map[bond.atom2] not in map[bond.atom1].connections:
             map[bond.atom1].add_connection(map[bond.atom2])
         if map[bond.atom1] not in map[bond.atom2].connections:
             map[bond.atom2].add_connection(map[bond.atom1])
+
+        
+
+    # TODO: Angles
+    # TODO: Dihedrals
 
     top.update_connection_list()
 
