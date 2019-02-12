06ef12efc43650e6d982ba5d6be78acfc41e071b
Alex Yang <ahy3nz@virginia.edu>
2019-02-12 17:46:22 -0600

    Add connection_type property to Connection, note that the __eq__ method still only compares sites (doesn't look at parametrization)

--- topology/core/connection.py
+++ topology/core/connection.py
@@ -1,6 +1,6 @@
 class Connection(object):
     """A simple connection between sites."""
-    def __init__(self, site1=None, site2=None, update=True):
+    def __init__(self, site1=None, site2=None, update=True, connection_type=None):
         if site1:
             self.site1 = site1
         if site2:
@@ -9,7 +9,17 @@ class Connection(object):
             site1.add_connection(site2)
             site2.add_connection(site1)
 
+        self._connection_type = connection_type
+
+    @property
+    def connection_type(self):
+        return self._connection_type
+
+    @connection_type.setter
+    def connection_type(self, val):
+        self._connection_type = val
 
     def __eq__(self, other):
+        # No comparison of connection_type in case of non-parametrization
         return ((self.site1 == other.site1 and self.site2 == other.site2) or
                 (self.site2 == other.site1 and self.site1 == other.site2))
