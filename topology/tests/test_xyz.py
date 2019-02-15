4274c5db03825ade8b3442bd8efbc3b32d049ef2
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-15 12:23:39 -0600

    Fix imports

--- topology/tests/test_xyz.py
+++ topology/tests/test_xyz.py
@@ -1,4 +1,5 @@
 import unyt as u
+import pytest
 
 from topology.formats.xyz import read_xyz
 from topology.utils.io import get_fn
@@ -18,8 +19,8 @@ def test_read_xyz():
     assert set([type(site.position) for site in top.site_list]) == {u.unyt_array}
     assert set([site.position.units for site in top.site_list]) == {u.nm}
 
-def test_wrong_n_atoms(self):
+def test_wrong_n_atoms():
     with pytest.raises(ValueError):
-        mb.load(get_fn('too_few_atoms.xyz'))
+        read_xyz(get_fn('too_few_atoms.xyz'))
     with pytest.raises(ValueError):
-        mb.load(get_fn('too_many_atoms.xyz'))
+        read_xyz(get_fn('too_many_atoms.xyz'))
