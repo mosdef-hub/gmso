8ff44203da1a0c47b60a1394c73609e09c2d286b
Alex Yang <ahy3nz@virginia.edu>
2019-02-15 15:57:38 -0600

    Fix loading to use topology, not mbuild. Include pytest import. Eiminate unnecessary arg in atest func

--- topology/tests/test_xyz.py
+++ topology/tests/test_xyz.py
@@ -1,3 +1,5 @@
+import pytest
+
 from topology.formats.xyz import read_xyz
 from topology.utils.io import get_fn
 
@@ -9,8 +11,8 @@ def test_read_xyz():
     top = read_xyz(get_fn('cu_block.xyz'))
     assert top.n_sites == 108
 
-def test_wrong_n_atoms(self):
+def test_wrong_n_atoms():
     with pytest.raises(ValueError):
-        mb.load(get_fn('too_few_atoms.xyz'))
+        read_xyz(get_fn('too_few_atoms.xyz'))
     with pytest.raises(ValueError):
-        mb.load(get_fn('too_many_atoms.xyz'))
+        read_xyz(get_fn('too_many_atoms.xyz'))
