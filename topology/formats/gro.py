622f19dc0f8c9c7d3e8ca56441e0dbe4200682d6
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-15 17:44:51 -0600

    Explicitly state units while populating topology

--- topology/formats/gro.py
+++ topology/formats/gro.py
@@ -1,4 +1,5 @@
 import numpy as np
+import unyt as u
 
 from topology.core.topology import Topology
 from topology.core.site import Site
@@ -25,7 +26,7 @@ def read_gro(filename):
             res_name = line[5:10]
             atom_name = line[10:15]
             atom_id = int(line[15:20])
-            coords[row] = np.array([
+            coords[row] = u.nm * np.array([
                 float(line[20:28]),
                 float(line[28:36]),
                 float(line[36:44]),
@@ -35,7 +36,7 @@ def read_gro(filename):
 
         # Box information
         line = gro_file.readline().split()
-        top.box = Box(np.array([float(val) for val in line[:3]]))
+        top.box = Box(u.nm * np.array([float(val) for val in line[:3]]))
 
         # Verify we have read the last line by ensuring the next line in blank
         line = gro_file.readline()
