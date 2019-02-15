93b9fe778e09f0b48fca561cce61850e0fd95426
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-15 11:50:47 -0600

    Fix typo

--- topology/tests/test_xyz.py
+++ topology/tests/test_xyz.py
@@ -5,7 +5,7 @@ from topology.utils.io import get_fn
 def test_read_xyz():
     top = read_xyz(get_fn('ethane.xyz'))
     assert top.n_sites == 8
-    assert top.n_connections == 8
+    assert top.n_connections == 0
     assert set([type(site.position) for site in top.site_list]) == {u.unyt_array}
     assert set([site.position.units for site in top.site_list]) == {u.nm}
 
