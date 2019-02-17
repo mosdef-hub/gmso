29e1e520fb20a68b0e4fcbf9dd931fa806138e64
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-17 15:25:23 -0600

    Test for wrong number of atoms and use allclose

--- topology/tests/test_gro.py
+++ topology/tests/test_gro.py
@@ -1,18 +1,26 @@
 import numpy as np
+import unyt as u
+import pytest
 
 from topology.formats.gro import read_gro
 from topology.utils.io import get_fn
-
+from topology.testing.utils import allclose
 
 def test_read_gro():
     top = read_gro(get_fn('acn.gro'))
 
     assert top.name == 'ACN'
     assert top.n_sites == 6
-    assert np.allclose(top.box.lengths, 4*np.ones(3))
+    assert allclose(top.box.lengths, 4*np.ones(3)*u.nm)
 
     top = read_gro(get_fn('350-waters.gro'))
 
     assert top.name == 'Generic title'
     assert top.n_sites == 1050
-    assert np.allclose(top.box.lengths, 2.20866*np.ones(3))
+    assert allclose(top.box.lengths, 2.20866*np.ones(3)*u.nm)
+
+def test_wrong_n_atoms():
+    with pytest.raises(ValueError):
+        read_gro(get_fn('too_few_atoms.gro'))
+    with pytest.raises(ValueError):
+        read_gro(get_fn('too_many_atoms.gro'))
