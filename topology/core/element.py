46bf7cac5dbaed271c3daa4ccac3d21d77e15219
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-24 21:56:46 -0600

    Add element repr

--- topology/core/element.py
+++ topology/core/element.py
@@ -10,6 +10,13 @@ class Element(object):
             if elem.name == name:
                 return elem
 
+    def __repr__(self):
+        descr = list('<Element ')
+        descr.append(self.name + ', ')
+        descr.append(self.symbol + ', ')
+        descr.append(str(self.mass) + ' amu>')
+        return ''.join(descr)
+
 Hydrogen = Element(name='hydrogen', symbol='H', mass=1.007947)
 Carbon = Element(name='carbon', symbol='C', mass=12.011)
 Oxygen = Element(name='oxygen', symbol='O', mass=15.999)
