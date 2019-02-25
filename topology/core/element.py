fe8e6a40cf00e8a49238f592ac0d050aa0b728c8
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-24 21:48:50 -0600

    Make a list of elements to be used for lookups

--- topology/core/element.py
+++ topology/core/element.py
@@ -5,6 +5,15 @@ class Element(object):
         self.symbol = symbol
         self.mass = mass
 
+    def lookup_from_name(self, name):
+        return [elem for elem in elements if elem.name == name][0]
+
 Hydrogen = Element(name='hydrogen', symbol='H', mass=1.007947)
 Carbon = Element(name='carbon', symbol='C', mass=12.011)
 Oxygen = Element(name='oxygen', symbol='O', mass=15.999)
+
+elements = [
+    Hydrogen,
+    Carbon,
+    Oxygen,
+]
