89ba9202f012891bb3bcfa7757d93fdd4129eaea
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-03-02 19:05:20 -0600

    Add some more tests

--- topology/tests/test_topology.py
+++ topology/tests/test_topology.py
@@ -1,13 +1,19 @@
+from copy import deepcopy
+
 import numpy as np
 import pytest
 import unyt as u
+import parmed as pmd
 
 from topology.core.topology import Topology
 from topology.core.site import Site
 from topology.core.connection import Connection
 from topology.core.box import Box
-from topology.tests.base_test import BaseTest
+from topology.core.atom_type import AtomType
+from topology.external.convert_parmed import from_parmed
+from topology.utils.io import get_fn
 from topology.testing.utils import allclose
+from topology.tests.base_test import BaseTest
 
 
 class TestTopology(BaseTest):
@@ -56,3 +62,39 @@ class TestTopology(BaseTest):
         assert top.positions().dtype == float
         assert top.positions().units == u.nm
         assert isinstance(top.positions(), u.unyt_array)
+
+    def test_eq(self, box, charge):
+        top = Topology(name='mytop')
+
+        assert top != box
+
+        diff_name = deepcopy(top)
+        diff_name.name = 'othertop'
+        assert top != diff_name
+
+        site = Site()
+        wrong_n_sites = deepcopy(top)
+        assert top == wrong_n_sites
+        top.add_site(site)
+        assert top != wrong_n_sites
+
+        wrong_position = deepcopy(top)
+        top.add_site(Site(position=u.nm*[0, 0, 0]))
+        wrong_position.add_site(Site(position=u.nm*[1, 1, 1]))
+        assert top != wrong_position
+
+        wrong_charge = deepcopy(top)
+        top.add_site(Site(charge=charge))
+        wrong_position.add_site(Site(charge=-1*charge))
+        assert top != wrong_charge
+
+        wrong_atom_type = deepcopy(top)
+        top.add_site(Site(atom_type=AtomType(nb_function='epsilon*sigma')))
+        wrong_atom_type.add_site(Site(atom_type=AtomType(nb_function='sigma')))
+        assert top != wrong_atom_type
+
+        top1 = from_parmed(pmd.load_file(get_fn('ethane.top'),
+                                         xyz=get_fn('ethane.gro')))
+        top2 = from_parmed(pmd.load_file(get_fn('ethane.top'),
+                                         xyz=get_fn('ethane.gro')))
+        assert top1 == top2
