486f1da71b857754d87c6cdd5bd87a027bad9a91
Alex Yang <ahy3nz@virginia.edu>
2019-03-16 13:33:36 -0500

    Define Bond as a sub-class of Connection, with some additional validation methods

--- topology/core/bond.py
+++ topology/core/bond.py
@@ -1,37 +1,43 @@
+from topology.core.site import Site
+from topology.core.connection import Connection
 from topology.core.bond_type import BondType
+from topology.exceptions import TopologyError
 
-class Bond(object):
-    """A 2-partner connection between sites."""
-    def __init__(self, site1=None, site2=None, update=True, connection_type=None):
-        if site1:
-            self.site1 = site1
-        if site2:
-            self.site2 = site2
-        if update:
-            site1.add_connection(site2)
-            site2.add_connection(site1)
+class Bond(Connection):
+    """A 2-partner connection between sites.
+    
+    Partners
+    --------
+    bond_partners: list of topology.Site
+        Should be length 2
+    connection_type : topology.BondType
 
-        if isinstance(connection_type, ConnectionType):
-            self._connection_type = connection_type
-        elif connection_type is None:
-            self._connection_type = None
-        else:
-            self._connection_type = ConnectionType(connection_type)
+    Notes
+    -----
+    Inherits some methods from Connection:
+        __eq__, __repr__, _validate methods
+    Addiitonal _validate methods are presented
+    """
 
-    @property
-    def connection_type(self):
-        return self._connection_type
+    def __init__(self, bond_partners=[], connection_type=None):
+        bond_partners = _validate_two_partners(bond_partners)
+        connection_type = _validate_bondtype(connection_type)
 
-    @connection_type.setter
-    def connection_type(self, connection_type):
-        if isinstance(connection_type, ConnectionType):
-            self._connection_type = connection_type
-        elif connection_type is None:
-            self._connection_type = None
-        else:
-            self._connection_type = ConnectionType(connection_type)
+        super(Bond, self).__init__(bond_partners=bond_partners, 
+                connection_type=connection_type)
 
-    def __eq__(self, other):
-        # No comparison of connection_type in case of non-parametrization
-        return ((self.site1 == other.site1 and self.site2 == other.site2) or
-                (self.site2 == other.site1 and self.site1 == other.site2))
+
+def _validate_two_partners(bond_partners):
+    """Ensure 2 partners are involved in Bond"""
+    if len(bond_partners) != 2:
+        raise TopologyError("Trying to create a Bond " 
+                "with {} bond partners". format(len(bond_partners)))
+    
+    return bond_partners
+
+
+def _validate_bondtype(ctype):
+    """Ensure connection_type is a BondType """
+    if not isinstance(ctype, BondType):
+        raise TopologyError("Supplied non-BondType {}".format(c_type))
+    return ctype
