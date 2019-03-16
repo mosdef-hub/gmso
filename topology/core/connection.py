a625341ecb79d341aef1d8f10f8c1e6be13aac68
Alex Yang <ahy3nz@virginia.edu>
2019-03-16 12:50:24 -0500

    Eq, setters, validation methods

--- topology/core/connection.py
+++ topology/core/connection.py
@@ -1,6 +1,6 @@
-import numpy as np
-import sympy
-import unyt as u
+from topology.core import Site
+from topology.exceptions import TopologyError
+from topology.core import Potential
 
 class Connection(object):
     """ An abstract object that lists bonded partners and their type """
@@ -8,8 +8,40 @@ class Connection(object):
         self._bond_partners = _validate_bond_partners(bond_partners)
         self._connection_type = _validate_connection_type(connection_type)
 
+    @property
+    def bond_partners(self):
+        return self._bond_partners
+
+    @bond_partners.setter(self, bond_partners):
+        self._bond_partners = _validate_bond_partners(bond_partners)
+
+    @property
+    def connection_type(self):
+        return self._connection_type
+
+    @connection_type.setter(self, ctype):
+        self_connection_type = _validate_connection_type(ctype)
+
     def __repr__(self):
         descr = '<{}-partner connection between'.format(len(self.bond_partners))
         descr += ' '.join([site for site in self.bond_partners])
         descr += ', type {}'.format(self.connection_type)
         return(descr)
+
+    def __eq__(self, other):
+        bond_partner_match = (self.bond_partners == other.bond_partners)
+        ctype_match = (self.connection_type == other.connection_type)
+        return all([bond_partner_match, ctype_match])
+
+
+
+def _validate_bond_partners(bond_partners):
+    for partner in bond_partners:
+        if not isinstance(partner, Site):
+            raise TopologyError("Supplied non-site {}".format(partner))
+    return bond_partners
+
+def _validate_connection_type(c_type):
+    if not isinstance(c_type, Potential):
+        raise TopologyError("Supplied non-Potential {}".format(c_type))
+    return c_type
