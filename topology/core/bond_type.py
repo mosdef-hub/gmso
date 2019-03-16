742e168be866ddaf3995de047e1d8dc6a2c9ebfd
Alex Yang <ahy3nz@virginia.edu>
2019-03-16 13:34:13 -0500

    BondType as a sub-class of Potential with some additional validate methods

--- topology/core/bond_type.py
+++ topology/core/bond_type.py
@@ -12,11 +12,14 @@ class BondType(Potential):
     parameters : dict
         {str, u.Unit}
     independent vars : set of str
+        For canonical purposes, r needs to be an indpeendnet variable
 
     Notes
     ----
     Inherits many functions from topology.Potential:
         __eq__, _validate functions
+    But we have specified an addiitonal validate method for canonicalizing
+        the independent varaible
     """
 
     def __init__(self,
@@ -28,10 +31,18 @@ class BondType(Potential):
                  },
                  independent_variables={'r'}):
 
+        independent_variables = _validate_bondtype_independent_var(
+                independent_variables)
         super(BondType, self).__init__(name=name, expression=expression,
                 parameters=parameters, independent_variables=independent_variables)
 
     def __repr__(self):
         return "<BondType {}, id {}>".format(self.name, id(self))
 
-        
+def _validate_bondtype_independent_var(independent_variables):
+    """For canonical purposes, ensure r is the independent varaible"""
+    if 'r' not in independent_variables:
+        raise TopologyError("BondType needs 'r' as independent_variable, "
+                            "you supplied independent_variables {}".format(
+                                independent_variables))
+    return independent_variables
