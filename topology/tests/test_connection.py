390a00d7bd43d044551053b9201d7e04418594d1
Alex Yang <ahy3nz@virginia.edu>
2019-03-16 17:07:05 -0500

    Refactor test connection for new design

--- topology/tests/test_connection.py
+++ topology/tests/test_connection.py
@@ -1,17 +1,54 @@
+import pytest
+
 from topology.core.connection import Connection
+from topology.core.potential import Potential
+from topology.core.bond import Bond
+from topology.core.bond_type import BondType
 from topology.core.site import Site
 from topology.tests.base_test import BaseTest
+from topology.exceptions import TopologyError
 
 
 class TestConnection(BaseTest):
-    def test_new_connect(self):
+    def test_connection_nonparametrized(self):
         site1 = Site(name='site1')
         site2 = Site(name='site2')
 
         assert site1.n_connections == 0
         assert site2.n_connections == 0
 
-        connect = Connection(site1=site1, site2=site2)
+        connect = Connection(bond_partners=[site1, site2])
 
         assert site1.n_connections == 1
         assert site2.n_connections == 1
+        assert connect.connection_type is None
+
+    def test_connection_parametrized(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+
+        assert site1.n_connections == 0
+        assert site2.n_connections == 0
+        c_type = Potential()
+
+        connect = Connection(bond_partners=[site1, site2], 
+                connection_type=c_type)
+
+        assert site1.n_connections == 1
+        assert site2.n_connections == 1
+        assert len(connect.bond_partners) == 2
+        assert connect.connection_type is not None
+
+    def test_connection_fake(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+        with pytest.raises(TopologyError):
+            connect = Connection(bond_partners=['fakesite1', 'fakesite2'])
+
+    def test_bond_fake_ctype(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+        with pytest.raises(TopologyError):
+            connect = Connection(bond_partners=[site1,site2],
+                    connection_type='Fake ctype')
+
