428638f127713b3df5b4b95f2252fed86d03a12e
Alex Yang <ahy3nz@virginia.edu>
2019-03-16 17:07:15 -0500

    Testing bond and bondtype

---
+++ topology/tests/test_bond.py
@@ -0,0 +1,53 @@
+import pytest
+
+from topology.core.connection import Connection
+from topology.core.bond import Bond
+from topology.core.bond_type import BondType
+from topology.core.site import Site
+from topology.tests.base_test import BaseTest
+from topology.exceptions import TopologyError
+
+
+class TestBond(BaseTest):
+    def test_bond_nonparametrized(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+
+        assert site1.n_connections == 0
+        assert site2.n_connections == 0
+
+        connect = Bond(bond_partners=[site1, site2])
+
+        assert site1.n_connections == 1
+        assert site2.n_connections == 1
+        assert connect.connection_type is None
+
+    def test_bond_parametrized(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+
+        assert site1.n_connections == 0
+        assert site2.n_connections == 0
+        bond_type = BondType()
+
+        connect = Bond(bond_partners=[site1, site2], 
+                connection_type=bond_type)
+
+        assert site1.n_connections == 1
+        assert site2.n_connections == 1
+        assert len(connect.bond_partners) == 2
+        assert connect.connection_type is not None
+
+    def test_bond_fake(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+        with pytest.raises(TopologyError):
+            connect = Bond(bond_partners=['fakesite1', 'fakesite2'])
+
+    def test_bond_fake_bondtype(self):
+        site1 = Site(name='site1')
+        site2 = Site(name='site2')
+        with pytest.raises(TopologyError):
+            connect = Bond(bond_partners=[site1,site2],
+                    connection_type='Fake bondtype')
+
