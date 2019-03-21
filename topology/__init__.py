bc79b54e18896760530e7c2d5f78cbe4958c9222
Alex Yang <ahy3nz@virginia.edu>
2019-03-21 10:13:25 -0500

    Modify init for simpler import statemntes

--- topology/__init__.py
+++ topology/__init__.py
@@ -1,2 +1,7 @@
 from .core.topology import Topology
 from .core.element import Element
+from .core.box import Box
+from .core.site import Site
+from .core.connection import Connection
+from .core.potential import Potential
+from .core.atom_type import AtomType
