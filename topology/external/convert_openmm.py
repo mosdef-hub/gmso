46158484495d49566a9f0a87ead5c34294a0e0bb
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-03-21 08:33:35 -0500

    Clean up docstring

--- topology/external/convert_openmm.py
+++ topology/external/convert_openmm.py
@@ -89,58 +89,63 @@ def to_system(topology,
         Cutoff method specified for OpenMM system.  Options supported
         are 'NoCutoff', 'CutoffNonPeriodic', 'CutoffPeriodic', 'PME',
         or Ewald objects from simtk.openmm.app.
-    nonbondedCutoff: unyt array
-        The nonbonded cutoff must either be a floating point number or
-        a unyt array
-    switchDistance: unyt array
+    nonbondedCutoff: unyt array or float
+        The nonbonded cutoff must either be a float or
+        a unyt array.  Float interpreted in units of nm
+    switchDistance: unyt array or float
         The distance at which the switching function is turned on for
         van der waals interactions.  This is ignored when no cutoff is
         used, and no switch is used if switchDistance is 0, negative,
-        or greater than the cutoff.
+        or greater than the cutoff. Float point interpreted in units
+        of nm
     constraints: 'None', 'app.HBonds', 'app.HAngles', or 'app.AllBonds'
         Type of constraints to add to the System (e.g., SHAKE).
-    rigidWater: bool=True
+    rigidWater: boolean, default=True
         If True, water is kept rigid regardless of constraint values.
-        False value is overriden if constarints is not 'None'.
-    implicitSolvent: 'None', 'app.HCT', 'app.OBC1', 'app.OBC2', 'app.GBn', 'app.GBn2'
-        The Generalized Born implicit solvent model to use. Default=None
-    implicitSolventKappa: float or 1/distance unyt array, default=None
+        False value is overriden if constraints is not None
+    implicitSolvent: 'None', 'app.HCT', 'app.OBC1', 'app.OBC2',
+                     'app.GBn', 'app.GBn2'. Default=None
+        The Generalized Born implicit solvent model to use.
+    implicitSolventKappa: 1/distance unyt array or float, default=None
         Debye kappa property related to modeling saltware conditions
         in GB.  It should have units of 1/distance (interpreted as
         1/nanometers if no units reported).  A value of 'None' means
         that kapps will be calculated from implicitSolventSaltConc.
-    implicitSolventSaltConc: float or amount/volume unyt array, default=0 moles/Liter
+    implicitSolventSaltConc: amount/volume unyt array or float,
+                             default=0 moles/Liter (moles/dm^3)
         if implicitSolventKappa is 'None', the kappa will be computed
         from salt concentration.  Units should be compatible with
         mol/L.
-    temperature: float or temperature Quantity, default=300 Kelvin
+    temperature: temperature unyt array or flow,
+                 default=300 Kelvin
         This is only used to compute kappa from
-        implicitySolventSaltConc.
+        implicitySolventSaltConc.  If not unit given, temperature will be
+        interpreted in units of Kelvin.
     soluteDielectric: float, default=1.0
         The dielectric constant of protein interior used in GB.
     solventDielectric: float, default=78.5
         The dielectric constant of water used in GB
-    useSASA: bool, default=False
+    useSASA: boolean, default=False
         If True, use the ACE non-polar solvation model.  Otherwise, no
         SASA-based nonpolar solvation model is used.
-    removeCMMotion: bool, default=True
+    removeCMMotion: boolean, default=True
         If True, the center-of-mass motion will be removed
         periodically during the simulation.  If False, it will not.
-    hydrogenMass: float or mass quantity, default=None
+    hydrogenMass: mass unyt array or float, default=None
         If not None, hydrogen masses will be changed to this mass and
         the difference subtracted from the attached heavy atom
         (hydrogen mass repartitioning).
     ewaldErrorTolerance: float, default=0.0005
         When using PME or Ewald, the Ewald parameters will be
         calculated from this value.
-    flexibleConstraints: bool, default=True
+    flexibleConstraints: boolean, default=True
         If False, the energies and forces from the constrained degrees
         of freedom will NOT be computed.  If True, they will but those
         degrees of freedom will *still* be constrained).
-    verbose: bool, default=False
+    verbose: boolean, default=False
         If True, the progress of this subroutine will be printed to
         stdout.
-    splitDihedrals: bool, default=False
+    splitDihedrals: boolean, default=False
         If True, the dihedrals will be split into two forces --
         propers and impropers.  This is primarily useful for debugging
         torsion parameter assignments.
