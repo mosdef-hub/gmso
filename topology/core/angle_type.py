04724b4d23cfcbc22357ca2aa9547b1cd8ea4307
Alex Yang <ahy3nz@virginia.edu>
2019-03-29 17:43:52 -0500

    New attributes and validation methods

--- topology/core/angle_type.py
+++ topology/core/angle_type.py
@@ -1,6 +1,7 @@
 import unyt as u
 
 from topology.core.potential import Potential
+from topology.core.atom_type import AtomType
 
 
 class AngleType(Potential):
@@ -15,6 +16,7 @@ class AngleType(Potential):
         See `Potential` documentation for more information
     independent vars : set of str
         See `Potential` documentation for more information
+    types : list of topology.AtomType
 
     Notes
     ----
@@ -29,11 +31,33 @@ class AngleType(Potential):
                      'k': 1000 * u.Unit('kJ / (deg**2)'),
                      'theta_eq': 180 * u.deg
                  },
-                 independent_variables={'theta'}):
+                 independent_variables={'theta'},
+                 types=[]):
 
         super(AngleType, self).__init__(name=name, expression=expression,
                 parameters=parameters, independent_variables=independent_variables)
 
+        self._types = _validate_three_atomtypes(types)
+
+    @property
+    def types(self):
+        return self._types
+
+    @types.setter
+    def types(self, val):
+       self._types = _validate_three_atomtypes(val)
+
     def __repr__(self):
         return "<AngleType {}, id {}>".format(self.name, id(self))
+
+def _validate_three_atomtypes(types):
+    """Ensure 3 partners are involved in BondType"""
+    if len(types) != 3:
+        raise TopologyError("Trying to create an AngleType "
+                "with {} constituent types". format(len(types)))
+    if not all([isinstance(t, AtomType) for t in types]):
+        raise TopologyError("Types passed to AngleType "
+                            "need to be topology.AtomTypes")
+
+    return types
 
--- topology/core/bond_type.py
+++ topology/core/bond_type.py
@@ -1,6 +1,7 @@
 import unyt as u
 
 from topology.core.potential import Potential
+from topology.core.atom_type import AtomType
 
 
 class BondType(Potential):
@@ -15,6 +16,7 @@ class BondType(Potential):
         See `Potential` documentation for more information
     independent vars : set of str
         see `Potential` documentation for more information
+    types : list of topology.AtomType
 
     Notes
     ----
@@ -29,11 +31,33 @@ class BondType(Potential):
                      'k': 1000 * u.Unit('kJ / (nm**2)'),
                      'r_eq': 0.14 * u.nm
                  },
-                 independent_variables={'r'}):
+                 independent_variables={'r'},
+                 types=[]):
 
         super(BondType, self).__init__(name=name, expression=expression,
                 parameters=parameters, independent_variables=independent_variables)
 
+        self._types = _validate_two_atomtypes(types)
+
+    @property
+    def types(self):
+        return self._types
+
+    @types.setter
+    def types(self, val):
+       self._types = _validate_two_atomtypes(val)
+
     def __repr__(self):
         return "<BondType {}, id {}>".format(self.name, id(self))
+
+def _validate_two_atomtypes(types):
+    """Ensure 2 partners are involved in BondType"""
+    if len(types) != 2:
+        raise TopologyError("Trying to create a BondType "
+                "with {} constituent types". format(len(types)))
+    if not all([isinstance(t, AtomType) for t in types]):
+        raise TopologyError("Types passed to BondType "
+                            "need to be topology.AtomTypes")
+
+    return types
 
