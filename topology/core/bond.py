53773fa740c340949caa37573a03ec9c0ad84fae
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-02 13:29:35 -0500

    Define hashes for connection and bond classes

--- topology/core/bond.py
+++ topology/core/bond.py
@@ -29,11 +29,17 @@ class Bond(Connection):
                 connection_type=connection_type)
 
     def __eq__(self, other):
-        if not self.connection_members == other.connection_members:
-            return False
-        if not self.connection_type == other.connection_type:
-            return False
-        return True
+        return hash(self) == hash(other)
+
+    def __hash__(self):
+        if self.connection_type:
+            return hash(
+                tuple(
+                    tuple(self.connection_type),
+                    tuple(self.connection_members),
+                )
+            )
+        return hash(tuple(self.connection_members))
 
 def _validate_two_partners(connection_members):
     """Ensure 2 partners are involved in Bond"""
