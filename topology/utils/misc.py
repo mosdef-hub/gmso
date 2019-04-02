885499f0c157d91897f3614fb503869cfa38288a
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-02 14:58:02 -0500

    Improve unyt_to_hashable

--- topology/utils/misc.py
+++ topology/utils/misc.py
@@ -3,6 +3,8 @@ import unyt as u
 
 def unyt_to_hashable(val):
     """Convert a unyt array or quantity to a hashable tuple."""
+    if val is None:
+        return val
     if isinstance(val, u.unyt_quantity):
         val = u.unyt_array(val.reshape(-1))
-    return hash(tuple(val.in_base().value))
+    return tuple(val.in_base().value)
