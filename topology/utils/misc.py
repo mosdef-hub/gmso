724c6e830af81143559e242277401724a57e3532
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-05 10:50:26 -0400

    Try to handle cases of 0-value unyt quantities

--- topology/utils/misc.py
+++ topology/utils/misc.py
@@ -5,6 +5,8 @@ def unyt_to_hashable(val):
     """Convert a unyt array or quantity to a hashable tuple."""
     if val is None:
         return val
+    if val.value == 0:
+        return tuple((val.reshape(-1).value))
     if isinstance(val, u.unyt_quantity):
         val = u.unyt_array(val.reshape(-1))
     return tuple(val.in_base().value)
