a19746d3374ae0bf48cdacb779793ed5d4e1ab2a
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-12 12:00:19 -0500

    Add LJ and Buckingham templates

--- topology/lib/potential_templates.py
+++ topology/lib/potential_templates.py
@@ -14,3 +14,29 @@ class PotentialTemplate(Potential):
             independent_variables=independent_variables,
             template=template,
         )
+
+class LennardJonesPotential(PotentialTemplate):
+    def __init__(self,
+                 name='LennardJonesPotential',
+                 expression='4*epsilon*((sigma/r)**12 - (sigma/r)**6)',
+                 independent_variables={'r'}):
+
+        super(PotentialTemplate, self).__init__(
+            name=name,
+            expression=expression,
+            independent_variables=independent_variables,
+            template=True,
+        )
+
+class BuckinghamPotential(PotentialTemplate):
+    def __init__(self,
+                 name='BuckinghamPotential',
+                 expression='a*exp(-b*r) - c*r**-6',
+                 independent_variables={'r'}):
+
+        super(PotentialTemplate, self).__init__(
+            name=name,
+            expression=expression,
+            independent_variables=independent_variables,
+            template=True,
+        )
