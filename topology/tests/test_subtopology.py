b3cfa6a0518c1509080189a3883a33c740ad5540
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-25 14:22:25 -0500

    Test init and setters

--- topology/tests/test_subtopology.py
+++ topology/tests/test_subtopology.py
@@ -5,7 +5,37 @@ from topology.core.subtopology import SubTopology
 from topology.tests.base_test import BaseTest
 
 
-class TestBond(BaseTest):
+class TestSubTopology(BaseTest):
+    def test_subtop_init(self):
+        default = SubTopology()
+        assert default.name == 'Sub-Topology'
+        assert default.parent is None
+        assert default.n_sites == 0
+
+        named = SubTopology(name='CoolSubTopology')
+        assert named.name == 'CoolSubTopology'
+        assert named.parent is None
+        assert named.n_sites == 0
+
+        myparent = Topology()
+        with_parent = SubTopology(parent=myparent)
+        assert with_parent.name == 'Sub-Topology'
+        assert with_parent.parent == myparent
+        assert with_parent.n_sites == 0
+
+    def test_subtop_setters(self):
+        subtop = SubTopology()
+        assert subtop.name == 'Sub-Topology'
+        assert subtop.parent is None
+        assert subtop.n_sites == 0
+
+        subtop.name = 'NewSubTopology'
+        assert subtop.name == 'NewSubTopology'
+
+        newparent = Topology()
+        subtop.parent = newparent
+        assert subtop.parent == newparent
+
     def test_subtop_add_site(self):
         subtop = SubTopology()
         site = Site()
