6e95952b09b4cf12b472e111a511822c4c8adf75
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-25 13:56:42 -0500

    Start adding parent to subtop

--- topology/core/subtopology.py
+++ topology/core/subtopology.py
@@ -2,12 +2,16 @@ import warnings
 
 from boltons.setutils import IndexedSet
 
+from topology.core.topology import Topology
+
 
 class SubTopology(object):
     """A sub-topology."""
-    def __init__(self, name="Topology"):
+    def __init__(self, name="Topology", parent=None):
         if name is not None:
             self._name = name
+        if parent is not None:
+            self._parent = _validate_parent(parent)
         self._sites = IndexedSet()
 
     @property
@@ -26,6 +30,14 @@ class SubTopology(object):
     def n_sites(self):
         return len(self.sites)
 
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = _validate_parent(parent)
+
     def add_site(self, site):
         if site in self.sites:
             warnings.warn("Redundantly adding Site {}".format(site))
@@ -36,3 +48,10 @@ class SubTopology(object):
         descr.append(self.name + ' ')
         descr.append('{:d} sites, '.format(self.n_sites))
         descr.append('id: {}>'.format(id(self)))
+
+
+def _validate_parent(parent):
+    if isinstance(parent, Topology):
+        return parent
+    else:
+        raise TypeError('Argument {} is not type Topology'.format(parent))
