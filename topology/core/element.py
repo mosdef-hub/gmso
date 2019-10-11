344d5ec3f829bb5c5298a50a6d8e9abffdd825dc
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-10-11 09:36:27 -0500

    Test more values of element_by_atomic_number
    Also raise an exception if element_by_atomic_number finds None

--- topology/core/element.py
+++ topology/core/element.py
@@ -1,7 +1,10 @@
-import unyt as u
 import warnings
 from re import sub
+
 import numpy as np
+import unyt as u
+
+from topology.exceptions import TopologyError
 
 
 class Element(object):
@@ -68,9 +71,9 @@ def element_by_atomic_number(atomic_number):
 
     Returns
     -------
-    matched_element : element.Element or None
+    matched_element : element.Element
         Return an element from the periodic table if we find a match,
-        otherwise return None
+        otherwise riase TopologyError
     """
     if isinstance(atomic_number, str):
         atomic_number_trimmed = int(sub('[a-z -]', '', atomic_number.lower()).lstrip('0'))
@@ -80,8 +83,9 @@ def element_by_atomic_number(atomic_number):
     else:
         atomic_number_trimmed = atomic_number
     matched_element = atomic_dict.get(atomic_number_trimmed)
+    if matched_element is None:
+        raise TopologyError(f'Failed to find an element with atomic number {atomic_number_trimmed}')
     return matched_element
-
 
 def element_by_mass(mass, exact=True):
     """Search for an element by its mass
