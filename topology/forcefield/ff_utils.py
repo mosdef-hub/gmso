a3e14d53ae969c2c62d308e57106e57ac9bdc3ab
Umesh Timalsina <umesh.timalsina@ioe.edu.np>
2019-12-08 17:39:03 -0600

    AtomType Definiton complete in xsd

--- topology/forcefield/ff_utils.py
+++ topology/forcefield/ff_utils.py
@@ -1,16 +1,23 @@
+import os
+
+
+from lxml import etree
+from lxml.etree import DocumentInvalid
+
 import unyt as u
+
 
 UNITS_MAP = {
     'amu': u.gram / u.mol
 }
 
-def validate(xml, schema):
-    """Validate a given xml file with a refrence schema"""
-    pass
 
-def mass_to_unyt(mass):
-    try:
-        unit = str(mass).split(' ')[1]
-        return float(str(mass)).split(' ')[0] * UNITS_MAP[unit]
-    except IndexError:
-        return float(mass) * UNITS_MAP['amu']
+def validate(xml_path, schema=None):
+    """Validate a given xml file with a refrence schema"""
+    if schema is None:
+        schema_path = os.path.join(os.path.abspath(__file__), 'schema', 'article-schema.xsd')
+
+    xml_doc = etree.parse(schema_path)
+    xmlschema = etree.XMLSchema(xml_doc)
+
+    xmlschema.assertValid
