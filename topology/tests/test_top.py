ad06e11754896a6cad09a40923ff99f6bed1986f
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2020-02-05 11:30:16 -0600

    Add top writer tests

--- topology/tests/test_top.py
+++ topology/tests/test_top.py
@@ -1,14 +1,54 @@
-import parmed as pmd
+import mbuild as mb
+import pytest
 
+import topology as topo
 from topology.formats.top import write_top
-from topology.external.convert_parmed import from_parmed
+from topology.external.convert_mbuild import from_mbuild
 from topology.tests.base_test import BaseTest
 from topology.utils.io import get_fn
+from topology.exceptions import EngineIncompatibilityError
 
 
 class TestTop(BaseTest):
     def test_write_top(self):
-        top = from_parmed(pmd.load_file(get_fn('ethane.top'),
-                                        xyz=get_fn('ethane.gro')))
+        ar = mb.Compound(name='Ar')
 
-        write_top(top, '/Users/mwt/tmp/out.top')
+        packed_system = mb.fill_box(
+            compound=ar,
+            n_compounds=200,
+            box=mb.Box([3, 3, 3]),
+        )
+
+        top = from_mbuild(packed_system)
+
+        ff = topo.ForceField(get_fn('ar.xml'))
+
+        for site in top.sites:
+            site.atom_type = ff.atom_types['Ar']
+
+        top.update_topology()
+
+        write_top(top, 'ar.top')
+
+    def test_bad_potential(self):
+        ar = mb.Compound(name='Ar')
+
+        packed_system = mb.fill_box(
+            compound=ar,
+            n_compounds=200,
+            box=mb.Box([3, 3, 3]),
+        )
+
+        top = from_mbuild(packed_system)
+
+        ff = topo.ForceField(get_fn('ar.xml'))
+
+        for site in top.sites:
+            site.atom_type = ff.atom_types['Ar']
+
+        top.update_topology()
+
+        top.atom_types[0].expression = 'sigma + epsilon'
+
+        with pytest.raises(EngineIncompatibilityError):
+            write_top(top, 'out.top')
