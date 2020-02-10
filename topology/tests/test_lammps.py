fb89e0d955a4569115c7037891a3acae70f02cdd
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-02-10 16:29:30 -0600

    Remove setting variable to topology_site

--- topology/tests/test_lammps.py
+++ topology/tests/test_lammps.py
@@ -10,56 +10,49 @@ class TestLammpsWriter(BaseTest):
         write_lammpsdata(top, filename='data.lammps')
 
     def test_write_lammps_triclinic(self, topology_site):
-        top = topology_site()
-        top.box = Box(lengths=[1,1,1], angles=[60,90,120])
-        write_lammpsdata(top, filename='data.triclinic')
+        topology_site().box = Box(lengths=[1,1,1], angles=[60,90,120])
+        write_lammpsdata(topology_site(), filename='data.triclinic')
 
     def test_read_lammps(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read_lammpsdata('data.lammps')
 
     def test_read_sites(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read = read_lammpsdata('data.lammps')
 
         assert read.box == Box(lengths=[1, 1, 1])
 
     def test_read_n_sites(self, topology_site):
-        top = topology_site(sites=4)
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(sites=4),
+                filename='data.lammps')
         read = read_lammpsdata('data.lammps')
 
         assert read.n_sites == 4
 
     def test_read_mass(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read = read_lammpsdata('data.lammps')
         masses = [i.mass for i in read.atom_types]
 
         assert masses == u.unyt_array(1, u.g)
 
     def test_read_mass(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read = read_lammpsdata('data.lammps')
         charge = [i.charge for i in read.atom_types]
 
         assert charge == u.unyt_array(0, u.C)
 
     def test_read_sigma(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read = read_lammpsdata('data.lammps')
         lj = [i.parameters for i in read.atom_types][0]
 
         assert lj['sigma'] == u.unyt_array(3, u.angstrom)
 
     def test_read_epsilon(self, topology_site):
-        top = topology_site()
-        write_lammpsdata(top, filename='data.lammps')
+        write_lammpsdata(topology_site(), filename='data.lammps')
         read = read_lammpsdata('data.lammps')
         lj = [i.parameters for i in read.atom_types][0]
 
