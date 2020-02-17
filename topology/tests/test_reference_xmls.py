a00efa1e46e4872834f152b33cf8c99b0ea76c96
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-02-17 12:50:42 -0600

    Address Parashara's changes

--- topology/tests/test_reference_xmls.py
+++ topology/tests/test_reference_xmls.py
@@ -58,13 +58,24 @@ class TestForceFieldFromXML(BaseTest):
             "4*epsilon*((sigma/r)**12 - (sigma/r)**6)",
             "0.5 * k * (r-r_eq)**2",
             "0.5 * k * (theta-theta_eq)**2",
-            "k * (1 + cos(n * theta - theta_0))",
             ]
         ]
 
-        #assert water.atom_types['opls_111'].charge.value == -0.834
-        #assert water.atom_types['opls_112'].charge.value == 0.417
+        assert water.atom_types['opls_111'].charge.value == -0.834
+        assert water.atom_types['opls_112'].charge.value == 0.417
 
         assert sympy.simplify(water.atom_types['opls_111'].expression - ref_exprs[0]) == 0
         assert sympy.simplify(water.atom_types['opls_112'].expression - ref_exprs[0]) == 0
         assert sympy.simplify(water.bond_types['opls_111~opls_112'].expression - ref_exprs[1]) == 0
+        assert sympy.simplify(water.angle_types['opls_112~opls_111~opls_112'].expression - ref_exprs[2]) == 0
+
+        assert allclose(water.atom_types['opls_111'].parameters['sigma'], 0.636386 * u.nm)
+        assert allclose(water.atom_types['opls_111'].parameters['epsilon'], 0.315061 * u.Unit('kJ/mol'))
+        assert allclose(water.atom_types['opls_112'].parameters['sigma'], 1.0 * u.nm)
+        assert allclose(water.atom_types['opls_112'].parameters['epsilon'], 0.0 * u.Unit('kJ/mol'))
+
+        assert allclose(water.bond_types['opls_111~opls_112'].parameters['r_eq'], 0.09572 * u.nm)
+        assert allclose(water.bond_types['opls_111~opls_112'].parameters['k'], 502416.0 * u.Unit('kJ/(mol*nm**2)'))
+
+        assert allclose(water.angle_types['opls_112~opls_111~opls_112'].parameters['theta_eq'], 1.824218134 * u.radian)
+        assert allclose(water.angle_types['opls_112~opls_111~opls_112'].parameters['k'], 682.02 * u.Unit('kJ/(mol*rad**2)'))
