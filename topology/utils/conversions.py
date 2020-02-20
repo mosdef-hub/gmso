9989343355d15d351b1a5dafb77e1cd7ba8d7756
Ryan S. DeFever <r.s.defever@gmail.com>
2020-02-20 15:44:22 -0500

    Accept and return DihedralType

--- topology/utils/conversions.py
+++ topology/utils/conversions.py
@@ -4,9 +4,10 @@ from topology.lib.potential_templates import RyckaertBellemansTorsionPotential
 from topology.lib.potential_templates import OPLSTorsionPotential
 from topology.exceptions import TopologyError
 
+import sympy
 import unyt as u
 
-def convert_opls_to_ryckaert(connection):
+def convert_opls_to_ryckaert(opls_connection_type):
     """Convert an OPLS dihedral to Ryckaert-Bellemans dihedral
 
     Equations taken/modified from:
@@ -18,11 +19,22 @@ def convert_opls_to_ryckaert(connection):
     phi_cis = 0 while RB torsions are defined as phi_trans = 0.
     """
 
-    f0 = connection.connection_type.parameters['k0']
-    f1 = connection.connection_type.parameters['k1']
-    f2 = connection.connection_type.parameters['k2']
-    f3 = connection.connection_type.parameters['k3']
-    f4 = connection.connection_type.parameters['k4']
+    valid_connection_type = False
+    if ( opls_connection_type.independent_variables ==
+         OPLSTorsionPotential().independent_variables ):
+        if sympy.simplify(opls_connection_type.expression -
+                          OPLSTorsionPotential().expression) == 0:
+            valid_connection_type = True
+    if not valid_connection_type:
+        raise TopologyError('Cannot use convert_opls_to_ryckaert '
+            'function to convert a ConnectionType that is not an '
+            'OPLSTorsionPotential')
+
+    f0 = opls_connection_type.parameters['k0']
+    f1 = opls_connection_type.parameters['k1']
+    f2 = opls_connection_type.parameters['k2']
+    f3 = opls_connection_type.parameters['k3']
+    f4 = opls_connection_type.parameters['k4']
 
     converted_params = {
             'c0' : (f2 + 0.5 * (f0 + f1 + f3)),
@@ -37,28 +49,40 @@ def convert_opls_to_ryckaert(connection):
     expression = RyckaertBellemansTorsionPotential().expression
     variables = RyckaertBellemansTorsionPotential().independent_variables
 
-    updated_connection_type = topo.DihedralType(
+    ryckaert_connection_type = topo.DihedralType(
             name=name,
             expression=expression,
             independent_variables=variables,
             parameters=converted_params)
 
-    connection.connection_type = updated_connection_type
+    return ryckaert_connection_type
 
-def convert_ryckaert_to_opls(connection):
-    """Convert Ryckaert-Bellemans dihedrals to OPLS
+def convert_ryckaert_to_opls(ryckaert_connection_type):
+    """Convert Ryckaert-Bellemans dihedral to OPLS
 
     NOTE: the conventions defining the dihedral angle are different
     for OPLS and RB torsions. OPLS torsions are defined with
     phi_cis = 0 while RB torsions are defined as phi_trans = 0.
     """
 
-    c0 = connection.connection_type.parameters['c0']
-    c1 = connection.connection_type.parameters['c1']
-    c2 = connection.connection_type.parameters['c2']
-    c3 = connection.connection_type.parameters['c3']
-    c4 = connection.connection_type.parameters['c4']
-    c5 = connection.connection_type.parameters['c5']
+    valid_connection_type = False
+    if ( ryckaert_connection_type.independent_variables ==
+         RyckaertBellemansTorsionPotential().independent_variables ):
+        if sympy.simplify(ryckaert_connection_type.expression -
+                RyckaertBellemansTorsionPotential().expression) == 0:
+            valid_connection_type = True
+    if not valid_connection_type:
+        raise TopologyError('Cannot use convert_ryckaert_to_opls '
+            'function to convert a ConnectionType that is not an '
+            'RyckaertBellemansTorsionPotential')
+
+
+    c0 = ryckaert_connection_type.parameters['c0']
+    c1 = ryckaert_connection_type.parameters['c1']
+    c2 = ryckaert_connection_type.parameters['c2']
+    c3 = ryckaert_connection_type.parameters['c3']
+    c4 = ryckaert_connection_type.parameters['c4']
+    c5 = ryckaert_connection_type.parameters['c5']
 
     if c5 != 0.0:
         raise TopologyError('Cannot convert Ryckaert-Bellemans dihedral '
@@ -76,11 +100,11 @@ def convert_ryckaert_to_opls(connection):
     expression = OPLSTorsionPotential().expression
     variables = OPLSTorsionPotential().independent_variables
 
-    updated_connection_type = topo.DihedralType(
+    opls_connection_type = topo.DihedralType(
             name=name,
             expression=expression,
             independent_variables=variables,
             parameters=converted_params)
 
-    connection.connection_type = updated_connection_type
+    return opls_connection_type
 
