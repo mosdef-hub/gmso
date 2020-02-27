5950507466d7713929a0853554ff645e2b00b469
Co Quach <daico007@gmail.com>
2020-02-27 15:35:32 -0600

    add atomtype, bondtype, angletype conversion helper code

--- gmso/external/convert_parmed.py
+++ gmso/external/convert_parmed.py
@@ -214,7 +214,7 @@ def from_parmed(structure):
     return top
 
 
-def to_parmed(top):
+def to_parmed(top, refer_type=True):
     """Convert a topology.Topology to a parmed.Structure
 
     At this point we only assume a three level structure for topology
@@ -223,10 +223,14 @@ def to_parmed(top):
     If we decide to support multiple level Subtopology in the future,
     this method will need some re-work. Tentative plan is to have the
     Parmed Residue to be equivalent to the Subtopology right above Site.
+
     Parameters
     ----------
     top : topology.Topology
         topology.Topology instance that need to be converted
+    refer_type : bool, optional, default=True
+        Whether or not to transfer AtomType, BondType, AngleTye,
+        and DihedralType information
 
     Returns
     -------
@@ -245,6 +249,9 @@ def to_parmed(top):
     # Maps
     subtop_map = dict() # Map site to subtop
     atom_map = dict() # Map site to atom
+    bond_map = dict() # Map top's bond to structure's bond
+    angle_map = dict() # Map top's angle to strucutre's angle
+    dihedral_map = dict() # Map top's dihedral to structure's dihedral
 
     # Set up unparametrized system
     # Build subtop_map (site -> top)
@@ -281,21 +288,43 @@ def to_parmed(top):
     # Create and add bonds to Parmed structure
     for bond in top.bonds:
         site1, site2 = bond.connection_members
-        bond = pmd.Bond(atom_map[site1], atom_map[site2])
-        structure.bonds.append(bond)
+        pmd_bond = pmd.Bond(atom_map[site1], atom_map[site2])
+        structure.bonds.append(pmd_bond)
+        bond_map[bond] = pmd_bond
+
+    # Create and add angles to Parmed structure
+    for angle in top.angles:
+        site1, site2, site3 = angle.connection_members
+        pmd_angle = pmd.Angle(atom_map[site1],
+                              atom_map[site2],
+                              atom_map[site3])
+        structure.angles.append(pmd_angle)
+        angle_map[angle] = pmd_angle
+
+    # Create and add dihedrals to Parmed structure
+    for dihedral in top.dihedrals:
+        site1, site2, site3, site4 = dihedral.connection_members
+        pmd_dihedral = pmd.Dihedral(atom_map[site1],
+                                    atom_map[site2],
+                                    atom_map[site3],
+                                    atom_map[site4])
+        structure.dihedrals.append(pmd_dihedral)
+
 
     # Set up structure for Connection Type conversion
     # Will need to work on these helper function later
-    """
-    if top.atom_types:
-        _convert_atom_types(top, structure, atom_map)
-    if top.bond_types:
-        _convert_bond_types(top, structure)
-    if top.angle_types:
-        _convert_angle_types(top, structure)
-    if top.dihedral_types:
-        _convert_dihedral_types(top, structure)
-    """
+
+    if refer_type:
+    # Need to add a warning if Topology does not have types information
+        if top.atom_types:
+            _convert_atom_types(top, structure, atom_map)
+        if top.bond_types:
+            _convert_bond_types(top, structure, bond_map)
+        if top.angle_types:
+            _convert_angle_types(top, structure)
+        if top.dihedral_types:
+            _convert_dihedral_types(top, structure)
+
     return structure
 
 
@@ -313,11 +342,11 @@ def _convert_atom_types(top, structure, atom_map):
         The destination parmed Structure
     """
     # Maps
-    type_map = dict()
+    atype_map = dict()
     for atom_type in top.atom_types:
         msg = "Atom type {} expression does not match Parmed AtomType default expression".format(atom_type.name)
-        assert site.atom_type.expression == parse_expr("4*epsilon*(-sigma**6/r**6 + sigma**12/r**12)"), msg
-        #Extract site's atom type information
+        assert atom_type.expression == parse_expr("4*epsilon*(-sigma**6/r**6 + sigma**12/r**12)"), msg
+        #Extract Topology atom type information
         atype_name = site.atom_type.name
         atype_charge = site.atom_type.charge.to("elementary_charge").value
         atype_sigma = site.atom_type.parameters[sigma].to("nm")
@@ -329,16 +358,16 @@ def _convert_atom_types(top, structure, atom_map):
                              atype_element.atomic_number, atype_charge)
         atype.set_lj_params(atype_epsilon, atype_rmin)
         # Type map to match AtomType to its name
-        type_map[atype_name] = atype
+        atype_map[atype_name] = atype
         
     for site in top.sites:
         #Assign atom_type to atom
-        atom = atom_map[site]
-        atom.type = site.atom_type.name
-        atom.atom_type = type_map[atom.type]
+        pmd_atom = atom_map[site]
+        pmd_atom.type = site.atom_type.name
+        pmd_atom.atom_type = atype_map[atom.type]
 
 
-def _convert_bond_types(top, structure):
+def _convert_bond_types(top, structure, bond_map):
     """Helper function to convert Topology BondType to Structure BondType
 
     This function will first check the BondType expression of Topology and make sure it match with the one default in Parmed.
@@ -351,8 +380,23 @@ def _convert_bond_types(top, structure):
     structure: parmed.Structure
         The destination parmed Structure
     """
-    
-    pass
+    btype_map = dict()
+    for bond_type in top.bond_types:
+        msg = "Bond type {} expression does not match Parmed BondType default expression".format(atom_type.name)
+        assert bond_type.expression == parse_expr("0.5 * k * (r-r_eq)**2"), msg
+        # Extract Topology bond_type information
+        btype_k =  0.5 * bond_type.parameters['k']
+        btype_r_eq = bond_type.parameters['r_eq']
+        # Create unique Parmed BondType object
+        btype = pmd.BondType(btype_k, btype_r_eq)
+        # Type map to match Topology BondType with Parmed BondType
+        btype_map[bond_type] = btype
+        
+    for bond in structure.bonds:
+        #Assign bond_type to bond
+        pmd_bond = bond_map[bond]
+        pmd_bond.type = btype_map[bond.bond_type]
+    structure.bond_types.claim()
 
 
 def _convert_angle_types(top, structure):
@@ -368,7 +412,23 @@ def _convert_angle_types(top, structure):
     structure: parmed.Structure
         The destination parmed Structure
     """
-    pass
+    agltype_map = dict()
+    for angle_type in top.angle_types:
+        msg = "Angle type {} expression does not match Parmed AngleType default expression".format(atom_type.name)
+        assert bond.bond_type.expression == parse_expr("0.5 * k * (theta-theta_eq)**2"), msg
+        # Extract Topology angle_type information
+        agltype_k = 0.5 * angle_type.parameters['k']
+        agltype_theta_eq = angle_type.parameters['theta_eq']
+        # Create unique Parmed AngleType object
+        agltype = pmd.AngleType(agltype_k, agltype_theta_eq)
+        # Type map to match Topology AngleType with Parmed AngleType
+        agltype_map[angle_type] = agltype
+
+    for angle in structure.angles:
+        #Assign angle_type to angle
+        pmd_angle = bond_map[angle]
+        pmd_angle.type = agltype_map[angle.angle_type]
+    structure.angle_types.claim()
 
 
 def _convert_dihedral_types(top, structure):
