e536177e45b7a3396131b72a538258326bf4a16c
Bradley Dice <bdice@bradleydice.com>
2020-03-03 23:48:17 -0500

    Update setup.py
    Co-Authored-By: Matt Thompson <matt.thompson@vanderbilt.edu>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 #####################################
-VERSION = "0.0.0"
+VERSION = "0.2.0"
 ISRELEASED = False
 if ISRELEASED:
     __version__ = VERSION
