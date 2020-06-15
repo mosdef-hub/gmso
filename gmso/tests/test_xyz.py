871fd9f3b12ad7243e01e4297d99143e62efd780
Arjun Bansal <54412158+Argon1999@users.noreply.github.com>
2020-06-15 12:54:55 -0400

    Replace assert allclose function with unyt.array.assert_allclose_unit

--- gmso/tests/test_xyz.py
+++ gmso/tests/test_xyz.py
@@ -4,7 +4,7 @@ import pytest
 from gmso.formats.xyz import read_xyz, write_xyz
 from gmso.tests.base_test import BaseTest
 from gmso.utils.io import get_fn
-from gmso.utils.testing import allclose
+from unyt.testing import assert_allclose_unit
 
 class TestXYZ(BaseTest):
     def test_read_xyz(self):
@@ -39,4 +39,4 @@ class TestXYZ(BaseTest):
 
         assert original_top.n_sites == new_top.n_sites
         assert original_top.n_connections == new_top.n_connections
-        assert allclose(original_top.positions, new_top.positions)
+        assert_allclose_unit(original_top.positions, new_top.positions, rtol=1e-5, atol=1e-8)
