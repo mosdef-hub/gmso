2acb0c60b141cb7e2b1310521abfc270611bf6b7
Co Quach <daico007@gmail.com>
2020-07-14 08:53:53 -0500

    Simplify element __eq__

--- gmso/core/element.py
+++ gmso/core/element.py
@@ -36,11 +36,7 @@ class Element(namedtuple('Element', 'atomic_number, name, symbol, mass')):
                                                                       self.atomic_number,
                                                                       self.mass)
     def __eq__(self, other):
-        return (self.__class__ == other.__class__ and
-        self.name == other.name and
-        self.symbol == other.symbol and
-        self.atomic_number == other.atomic_number and
-        self.mass == self.mass)
+        return hash(self) == hash(other)
 
     def __hash__(self):
         return hash((self.name, self.symbol,
