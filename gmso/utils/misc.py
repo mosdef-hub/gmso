2c10c42f3d538fe8229cc30ba0fe43cd13596035
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2020-10-07 17:26:06 -0700

    Cache unit conversions when hashing

--- gmso/utils/misc.py
+++ gmso/utils/misc.py
@@ -2,25 +2,27 @@ import unyt as u
 from unyt.exceptions import UnitConversionError
 
 
+UNYT_CACHE = dict()
+
 def unyt_to_hashable(unyt_or_unyt_iter):
     """Convert a (list of) unyt array or quantity to a hashable tuple."""
     if unyt_or_unyt_iter is None:
         return unyt_or_unyt_iter
-    hash_coll = []
+
     if isinstance(unyt_or_unyt_iter, list):
-        for val in unyt_or_unyt_iter:
-            hash_coll.append(_unyt_to_hashable_single(val))
-        return tuple(hash_coll)
+        hash_coll = tuple(_unyt_to_hashable_single(val) for val in unyt_or_unyt_iter)
+        return hash_coll
     else:
         return _unyt_to_hashable_single(unyt_or_unyt_iter)
 
 
 def _unyt_to_hashable_single(val):
     if isinstance(val, u.unyt_quantity):
-        if val.units == u.elementary_charge and val.value == 0:
-            val.convert_to_units(u.coulomb)
-        val = u.unyt_array(val.reshape(-1))
-        return tuple(val.in_base().value)
+        try:
+            return val.value * UNYT_CACHE[val.units]
+        except KeyError:
+            UNYT_CACHE[val.units] = val.units.base_value
+            return val.value * UNYT_CACHE[val.units]
     else:
         return None
 
