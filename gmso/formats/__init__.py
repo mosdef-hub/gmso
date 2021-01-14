3d6e35b2922ce9e66e3a3e556013447b74a1220b
CalCraven <nicholas.c.craven@vanderbilt.edu>
2021-01-14 12:11:14 -0600

    Add functions to clean up networkx plotting, and plot missing angle and bond types

--- gmso/formats/__init__.py
+++ gmso/formats/__init__.py
@@ -3,4 +3,4 @@ from .gro import read_gro, write_gro
 from .gsd import write_gsd
 from .xyz import read_xyz, write_xyz
 from .lammpsdata import write_lammpsdata
-from .networx import *
+from .networkx import *
