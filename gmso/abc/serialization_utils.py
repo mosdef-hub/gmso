81ce6e68dd325a0c1e9722ca85284f123c5bff38
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-02-24 16:12:55 -0600

    WIP- Add docstrings to unyt_conversion functions

--- gmso/abc/serialization_utils.py
+++ gmso/abc/serialization_utils.py
@@ -11,6 +11,7 @@ __all__ = [
 
 
 def unyt_to_dict(unyt_qt: Union[u.unyt_array, u.unyt_quantity]) -> dict:
+    """Convert a unyt qunatity into json serializable dictionary"""
     if not isinstance(unyt_qt, u.unyt_array):
         raise ValueError(
             'Please provide a value of type unyt array or unyt quantity'
@@ -24,7 +25,8 @@ def unyt_to_dict(unyt_qt: Union[u.unyt_array, u.unyt_quantity]) -> dict:
         }
 
 
-def dict_to_unyt(dict_obj):
+def dict_to_unyt(dict_obj) -> None:
+    """Recursively convert values of dictionary containing units information to unyt quantities"""
     for key, value in dict_obj.items():
         if isinstance(value, dict):
             if 'array' not in value and 'unit' not in value:
