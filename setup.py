5543f206b9485d18b88391823179a6dac54c6380
Co Quach <daico007@gmail.com>
2021-04-20 12:09:02 -0500

    Bump to version 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 #####################################
-VERSION = "0.4.0"
+VERSION = "0.5.0"
 ISRELEASED = False
 if ISRELEASED:
     __version__ = VERSION
