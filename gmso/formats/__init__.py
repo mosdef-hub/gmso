1e7cd862dec8aee137353ad693e8749e06e27543
CalCraven <nicholas.c.craven@vanderbilt.edu>
2021-05-06 10:24:31 -0500

    fix __init__.py for importing networkx functionality

--- gmso/formats/__init__.py
+++ gmso/formats/__init__.py
@@ -3,5 +3,5 @@ from .gro import read_gro, write_gro
 from .gsd import write_gsd
 from .xyz import read_xyz, write_xyz
 from .lammpsdata import write_lammpsdata
-from .networkx import interactive_networkx_atomtypes, interactive_networkx_bonds, 
-                      interactive_networkx_angles, interactive_networkx_dihedrals
+from .networkx import (interactive_networkx_atomtypes, interactive_networkx_bonds, 
+interactive_networkx_angles, interactive_networkx_dihedrals)
