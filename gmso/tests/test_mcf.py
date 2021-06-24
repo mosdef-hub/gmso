e601e2876965f26794d8eaef9a400465de28b5fd
Ryan S. DeFever <r.s.defever@gmail.com>
2021-06-24 18:00:57 -0400

    Working to make MCF writer feature complete. Tests in progress.
    * Add fixed angle support
    * Fix charge sign error (gmso-wide)
    * Update ring identification to handle fused systems (mbuild #744)
    * Increase floating point accuracy
    * Increase element type length and atom type length
    * Correctly write atom indices for bonds/angles/dihedrals/impropers

--- gmso/tests/test_mcf.py
+++ gmso/tests/test_mcf.py
@@ -156,3 +156,60 @@ class TestMCF(BaseTest):
         assert np.allclose(float(mcf_data[-4][1]), 0.4)
         assert np.allclose(float(mcf_data[-4][2]), 0.6)
         assert np.allclose(float(mcf_data[-4][3]), 1.0)
+
+    def test_typed_ethane(self, typed_ethane):
+        write_mcf(typed_ethane, "ethane.mcf")
+
+        mcf_data = []
+        with open("ethane.mcf") as f:
+            for line in f:
+                mcf_data.append(line.strip().split())
+
+        for idx, line in enumerate(mcf_data):
+            if len(line) > 1:
+                if line[1] == "Atom_Info":
+                    atom_section_start = idx
+            if len(line) > 1:
+                if line[1] == "Bond_Info":
+                    bond_section_start = idx
+            if len(line) > 1:
+                if line[1] == "Angle_Info":
+                    angle_section_start = idx
+            if len(line) > 1:
+                if line[1] == "Dihedral_Info":
+                    dihedral_section_start = idx
+
+        # Check a some atom info
+        assert mcf_data[atom_section_start + 1][0] == "1"
+        assert mcf_data[atom_section_start + 2][1] == "Xe"
+        assert mcf_data[atom_section_start + 2][2] == "Xe"
+        assert mcf_data[atom_section_start + 2][5] == "Mie"
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][3]),
+            top.sites[0].mass.in_units(u.amu).value,
+        )
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][4]),
+            top.sites[0].charge.in_units(u.elementary_charge).value,
+        )
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][6]),
+            (top.sites[0].atom_type.parameters["epsilon"] / u.kb)
+                .in_units(u.K)
+                .value,
+                )
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][7]),
+            top.sites[0]
+                .atom_type.parameters["sigma"]
+                .in_units(u.Angstrom)
+                .value,
+                )
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][8]),
+            top.sites[0].atom_type.parameters["n"],
+        )
+        assert np.isclose(
+            float(mcf_data[atom_section_start + 2][9]),
+            top.sites[0].atom_type.parameters["m"],
+        )
