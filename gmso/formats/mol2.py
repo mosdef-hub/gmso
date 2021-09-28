365e14478748e5ad4cbe595149debe93a69dd86c
CalCraven <54594941+CalCraven@users.noreply.github.com>
2021-09-28 14:28:25 -0500

    Improvements to pythonic statements in return statements
    Co-authored-by: Umesh Timalsina <umesh.timalsina@vanderbilt.edu>

--- gmso/formats/mol2.py
+++ gmso/formats/mol2.py
@@ -208,12 +208,9 @@ def _parse_record_type_indicator(f, line, topology, site_type):
 
 def _if_end_of_rti(line):
     """Check if line in an rti is at the end of the section."""
-    if (
-        "@" not in line
-        and not line == "\n"
-        and line
-        and not line.strip().startswith("#")
-    ):
-        return True
-    else:
-        return False
+return (
+            "@" not in line
+            and not line == "\n"
+            and line
+            and not line.strip().startswith("#")
+        )
