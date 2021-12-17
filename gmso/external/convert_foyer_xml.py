2af112e00a79e053604eb122cf7a039d1d2e2f4f
bc118 <crawfordb118@gmail.com>
2021-12-17 17:30:38 -0500

    changed RB from phi to psi in convert_foyer_xml.py

--- gmso/external/convert_foyer_xml.py
+++ gmso/external/convert_foyer_xml.py
@@ -478,7 +478,7 @@ def _write_rb_torsions(forcefield, ff_kwargs):
         forcefield,
         "DihedralTypes",
         attrib_dict={
-            "expression": "c0 * cos(psi)**0 + c1 * cos(psi)**1 + "
+            "expression": "c0 + c1 * cos(psi)**1 + "
             "c2 * cos(psi)**2 + c3 * cos(psi)**3 + "
             "c4 * cos(psi)**4 + c5 * cos(psi)**5",
         },
