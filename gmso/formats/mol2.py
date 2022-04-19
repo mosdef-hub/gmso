698542bfbdaa53cc814cce0ef996e1a17f8d5642
Co Quach <daico007@gmail.com>
2022-04-19 22:10:06 +0700

    add Cal review, fix a few typo

--- gmso/formats/mol2.py
+++ gmso/formats/mol2.py
@@ -33,6 +33,7 @@ def from_mol2(filename, site_type="atom"):
 
     Notes
     -----
+    The position of atom in the mol2 file is assumed to be in Angstrom.
     It may be common to want to create an mBuild compound from a mol2 file. This is possible
     by installing [mBuild](https://mbuild.mosdef.org/en/stable/index.html)
     and converting using the following python code:
@@ -49,52 +50,76 @@ def from_mol2(filename, site_type="atom"):
     # Initialize topology
     topology = Topology(name=mol2path.stem)
     # save the name from the filename
-    with open(mol2path, "r") as f:
-        line = f.readline()
-        with open(filename, "r") as f:
-            fcontents = f.readlines()
-        sections = dict()
-        key = "meta"
-        for line in fcontents:
-            if "@<TRIPOS>" in line:
-                section_key = line.strip("\n")
-                sections[section_key] = list()
-            else:
-                sections[section_key].append(line)
+    with open(filename, "r") as f:
+        fcontents = f.readlines()
+    sections = dict()
+    for line in fcontents:
+        if "@<TRIPOS>" in line:
+            section_key = line.strip("\n")
+            sections[section_key] = list()
+        else:
+            sections[section_key].append(line)
 
-        supported_rti = {
-            "@<TRIPOS>ATOM": _parse_atom,
-            "@<TRIPOS>BOND": _parse_bond,
-            "@<TRIPOS>CRYSIN": _parse_box,
-            "@<TRIPOS>FF_PBC": _parse_box,
-        }
-        for section in sections:
-            if section not in supported_rti:
-                warnings.warn(
-                    f"The record type indicator {section} is not supported."
-                )
-            else:
-                supported_rti[section](
-                    topology, sections[section], site_type="atom"
-                )
+    _parse_site = _parse_lj if site_type == "lj" else _parse_atom
+    supported_rti = {
+        "@<TRIPOS>ATOM": _parse_site,
+        "@<TRIPOS>BOND": _parse_bond,
+        "@<TRIPOS>CRYSIN": _parse_box,
+        "@<TRIPOS>FF_PBC": _parse_box,
+    }
+    for section in sections:
+        if section not in supported_rti:
+            warnings.warn(
+                f"The record type indicator {section} is not supported."
+            )
+        else:
+            supported_rti[section](
+                topology, sections[section], site_type="atom"
+            )
 
     topology.update_topology()
     # TODO: read in parameters to correct attribute as well. This can be saved in various rti sections.
     return topology
 
 
-def _parse_atom(top, section, site_type="atom"):
-    """Parse atom information from the mol2 file."""
-    parse_ele = {
-        "lj": lambda ele: None,
-        "atom": lambda ele: element_by_symbol(ele)
-        if element_by_symbol(ele)
-        else element_by_name(ele),
-    }
+def _parse_lj(top, section):
+    """Parse atom of lj style from mol2 file."""
     for line in section:
         cont = line.split()
         position = [float(x) for x in cont[2:5]] * u.Å
-        element = parse_ele[site_type](cont[5])
+
+        try:
+            charge = float(cont[8])
+        except IndexError:
+            warnings.warn(
+                f"No charge was detected for site {cont[1]} with index {cont[0]}"
+            )
+
+        atom = Atom(
+            name=cont[1],
+            position=position.to("nm"),
+            charge=charge,
+            residue_name=cont[7],
+            residue_number=int(cont[6]),
+        )
+        top.add_site(atom)
+
+
+def _parse_atom(top, section):
+    """Parse atom information from the mol2 file."""
+    parse_ele = (
+        lambda ele: element_by_symbol(ele)
+        if element_by_symbol(ele)
+        else element_by_name(ele),
+    )
+    for line in section:
+        cont = line.split()
+        position = [float(x) for x in cont[2:5]] * u.Å
+        element = parse_ele(cont[5])
+
+        if not element:
+            warnings.warn(f"Could not parse the element of {cont[5]}")
+
         try:
             charge = float(cont[8])
         except IndexError:
@@ -106,6 +131,7 @@ def _parse_atom(top, section, site_type="atom"):
             name=cont[1],
             position=position.to("nm"),
             element=element,
+            charge=charge,
             residue_name=cont[7],
             residue_number=int(cont[6]),
         )
