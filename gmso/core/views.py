d5114d9771088c00a80bc3dd86cec669b77b7ade
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2022-04-28 17:05:53 -0500

    Some docstring

--- gmso/core/views.py
+++ gmso/core/views.py
@@ -21,6 +21,7 @@ potential_attribute_map = {
 
 
 def get_identifier(potential):
+    """Get identifier for a topology potential."""
     if isinstance(potential, AtomType):
         return potential.name
     if isinstance(potential, (BondType, AngleType, DihedralType, ImproperType)):
@@ -28,6 +29,7 @@ def get_identifier(potential):
 
 
 def unique_potentials(potential_types):
+    """Filter unique potentials based on pre-defiend identifiers."""
     visited = defaultdict(set)
     for potential_type in potential_types:
         identifier = get_identifier(potential_type)
@@ -45,13 +47,19 @@ default_filters = {PotentialFilters.UNIQUE_NAME_CLASS: unique_potentials}
 
 
 class TopologyPotentialView:
-    """A potential view based on different filters for a topology potentials.
+    """A potential view based on different filters for a topology's potentials.
 
     Parameters
     ----------
     iterator: typing.Iterator, required=True
         An iterator of either topology sites or connections from which to extract potentials from
 
+    Notes
+    -----
+    The implementation of this class has been inspired by networkx.classes.reportviews.NodeView by extending
+    the idea of a view with filteration capabilities. See the source for NodeView for further details
+
+    https://github.com/networkx/networkx/blob/12c1a00cd116701a763f7c57c230b8739d2ed085/networkx/classes/reportviews.py#L115-L279
     """
 
     def __init__(self, iterator):
@@ -65,16 +73,43 @@ class TopologyPotentialView:
             if potential is item:
                 return j
 
-    def yield_view(self, filter_by=None):
+    def yield_view(self, filter_by=None, repeat=False):
+        """Yield a view of the potentials of the iterator provided.
+
+        Parameters
+        ----------
+        filter_by: str or func, default=None
+            If provided, filter the collected potentials by some function
+            see, default_filters for names of the default potential filters
+
+        repeat: bool, default=False
+            If true, repeat same objects in the iterator
+
+        Yields
+        ------
+        gmso.core.ParametricPotential
+            An instance of gmso.core.ParametricPotential from the attributes of Sites/Connections
+            in the iterator.
+
+        Notes
+        -----
+        It is allowed that multiple sites/connections point to the same parametric potential
+        by design. By default this function will just return a single object even if they are
+        referenced by multiple sites or connections, if so isn't desired, set the argument
+        repeat=True.
+        """
         visited = set()
         if not filter_by:
             for item in self.iterator:
                 potential = getattr(
                     item, potential_attribute_map[type(item)]
                 )  # Since this use is internal, KeyErrors N/A
-                if potential and id(potential) not in visited:
-                    visited.add(id(potential))
+                if repeat and potential:
                     yield potential
+                else:
+                    if potential and id(potential) not in visited:
+                        visited.add(id(potential))
+                        yield potential
         else:
             collected_potentials = filter(
                 lambda p: p is not None,
@@ -88,7 +123,7 @@ class TopologyPotentialView:
                 yield item
 
     def __call__(self, filter_by=None):
-
+        """The call method, for turning property decorators into functions"""
         if filter_by in default_filters:
             filter_by = default_filters[filter_by]
 
