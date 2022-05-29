dcd8bd9325b80e401ab0a511aced816c963a68f2
CalCraven <nicholas.c.craven@vanderbilt.edu>
2022-05-29 13:17:57 -0500

    Add test for number of impropers from NNdimethylformamide molecule

--- gmso/tests/test_convert_parmed.py
+++ gmso/tests/test_convert_parmed.py
@@ -335,3 +335,9 @@ class TestConvertParmEd(BaseTest):
         for gmso_atom, pmd_atom in zip(top.sites, pentane_ua_parmed.atoms):
             assert gmso_atom.element is None
             assert pmd_atom.element == 0
+
+    def test_from_parmed_impropers(self):
+        mol = "NN-dimethylformamide"
+        pmd_structure = pmd.load_file(get_fn("{}.top".format(mol)), xyz=get_fn("{}.gro".format(mol)), parametrize=False)
+        impropers = [dihedral for dihedral in pmd_structure.dihedrals if dihedral.improper]
+        assert len(impropers) == 2
