d7b08d6df3d28b9a379d85af0de3dd565aac3e98
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2022-06-06 12:35:59 -0500

    Further testing isomorphism

--- gmso/tests/parameterization/test_parameterization_options.py
+++ gmso/tests/parameterization/test_parameterization_options.py
@@ -1,3 +1,5 @@
+import random
+
 import forcefield_utilities as ffutils
 import mbuild as mb
 import pytest
@@ -29,9 +31,34 @@ class TestParameterizationOptions(BaseTest):
         apply(ethane_methane_top, {"Ethane": opls, "Methane": opls})
 
     def test_isomporhic_speedups(self, ethane_box_with_methane, oplsaa_gmso):
+        ethane_box_with_methane.identify_connections()
         apply(
             ethane_box_with_methane,
             oplsaa_gmso,
             identify_connections=False,
             identify_connected_components=True,
         )
+
+        ethane_subtops = list(
+            filter(
+                lambda subtop: subtop.name == "Ethane",
+                ethane_box_with_methane.subtops,
+            )
+        )
+        methane_subtops = list(
+            filter(
+                lambda subtop: subtop.name == "Methane",
+                ethane_box_with_methane.subtops,
+            )
+        )
+        ethane_a = random.choice(ethane_subtops)
+        ethane_b = random.choice(ethane_subtops)
+        for atom_a, atom_b in zip(ethane_a.sites, ethane_b.sites):
+            assert atom_a.atom_type == atom_b.atom_type
+            assert atom_a.atom_type is not None
+
+        methane_a = random.choice(methane_subtops)
+        methane_b = random.choice(methane_subtops)
+        for atom_a, atom_b in zip(methane_a.sites, methane_b.sites):
+            assert atom_a.atom_type == atom_b.atom_type
+            assert atom_a.atom_type is not None
