d961ab1966a0758ef876a7d7576a17269c9037a6
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2022-07-26 21:32:18 -0600

    adding unit test that checks contents of gsd file

--- gmso/tests/test_gsd.py
+++ gmso/tests/test_gsd.py
@@ -1,3 +1,5 @@
+import gsd.hoomd
+import mbuild as mb
 import pytest
 import unyt as u
 
@@ -13,6 +15,21 @@ if has_parmed:
 @pytest.mark.skipif(not has_gsd, reason="gsd is not installed")
 @pytest.mark.skipif(not has_parmed, reason="ParmEd is not installed")
 class TestGsd(BaseTest):
+    def test_write_gsd_untyped(self):
+        comp = mb.load("CCCC", smiles=True)
+        system = mb.fill_box(comp, n_compounds=3, density=100)
+        top = from_mbuild(system)
+        top.identify_connections()
+        top.save("out.gsd")
+        with gsd.hoomd.open("out.gsd") as traj:
+            snap = traj[0]
+            assert all([i in snap.particles.types for i in ["C", "H"]])
+            assert all([i in snap.bonds.types for i in ["C-C", "C-H"]])
+            assert all([i in snap.angles.types for i in ["C-C-C", "C-C-H"]])
+            assert all(
+                    [i in snap.dihedrals.types for i in ["C-C-C-C", "C-C-C-H"]]
+            )
+
     def test_write_gsd(self, hierarchical_compound):
         top = from_mbuild(hierarchical_compound)
         top.save("out.gsd")
