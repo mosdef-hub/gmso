dfda87f1a353db0f5369b5113ae84e0b9c90381f
Chris Jones <50423140+chrisjonesBSU@users.noreply.github.com>
2022-08-03 13:38:09 -0600

    Fix mass and charge in GSD writer; Add angles and dihedrals to GSD writer (#680)
    * Set default mass and charge to 0.0
    
    * fix typo in doc string
    
    * fix attribute error when setting names of bonds
    
    * trying to get gsd writer to work when not using parmed
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    for more information, see https://pre-commit.ci
    
    * fix naming when bond member is an AtomType
    
    * fix angle writer, use f-strings
    
    * rewrite bond parser to use same process as angles
    
    * remove gsd import; change hoomd version in doc strings
    
    * fix write_dihedrals
    
    * re-use old dihedral sorting logic, remove commented out dihedral lines
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    for more information, see https://pre-commit.ci
    
    * change default mass and charge values back to None; fix handling of None mass and charges in gsd writer
    
    * add missing f in warning message
    
    * remove periods for consistency
    
    * fix issue of setting typeid index values
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    for more information, see https://pre-commit.ci
    
    * adding unit test that checks contents of gsd file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    for more information, see https://pre-commit.ci
    
    * minor change in docstring gmso/formats/gsd.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Co Quach <43968221+daico007@users.noreply.github.com>

--- gmso/tests/test_gsd.py
+++ gmso/tests/test_gsd.py
@@ -1,6 +1,9 @@
+import gsd.hoomd
+import mbuild as mb
 import pytest
 import unyt as u
 
+from gmso.external.convert_mbuild import from_mbuild
 from gmso.external.convert_parmed import from_parmed
 from gmso.tests.base_test import BaseTest
 from gmso.utils.io import get_fn, has_gsd, has_parmed, import_
@@ -12,12 +15,29 @@ if has_parmed:
 @pytest.mark.skipif(not has_gsd, reason="gsd is not installed")
 @pytest.mark.skipif(not has_parmed, reason="ParmEd is not installed")
 class TestGsd(BaseTest):
-    # TODO: Have these tests not depend on parmed
-    def test_write_gsd(self):
+    def test_write_gsd_untyped(self):
+        comp = mb.load("CCCC", smiles=True)
+        system = mb.fill_box(comp, n_compounds=3, density=100)
+        top = from_mbuild(system)
+        top.identify_connections()
+        top.save("out.gsd")
+        with gsd.hoomd.open("out.gsd") as traj:
+            snap = traj[0]
+            assert all([i in snap.particles.types for i in ["C", "H"]])
+            assert all([i in snap.bonds.types for i in ["C-C", "C-H"]])
+            assert all([i in snap.angles.types for i in ["C-C-C", "C-C-H"]])
+            assert all(
+                [i in snap.dihedrals.types for i in ["C-C-C-C", "C-C-C-H"]]
+            )
+
+    def test_write_gsd(self, hierarchical_compound):
+        top = from_mbuild(hierarchical_compound)
+        top.save("out.gsd")
+
+    def test_write_gsd_pmd(self):
         top = from_parmed(
             pmd.load_file(get_fn("ethane.top"), xyz=get_fn("ethane.gro"))
         )
-
         top.save("out.gsd")
 
     def test_write_gsd_non_orthogonal(self):
@@ -25,5 +45,4 @@ class TestGsd(BaseTest):
             pmd.load_file(get_fn("ethane.top"), xyz=get_fn("ethane.gro"))
         )
         top.box.angles = u.degree * [90, 90, 120]
-
         top.save("out.gsd")
