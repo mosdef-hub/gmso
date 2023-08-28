827d4908af07992390f8df9f329f564a111b2c80
chrisjonesbsu <chrisjones4@u.boisestate.edu>
2023-08-28 11:03:15 -0600

    remove commented out line

--- gmso/external/convert_mbuild.py
+++ gmso/external/convert_mbuild.py
@@ -272,7 +272,6 @@ def _parse_site(site_map, particle, search_method, infer_element=False):
         ele = search_method(particle.name) if infer_element else None
 
     charge = particle.charge * u.elementary_charge if particle.charge else None
-    #mass = particle.mass * u.amu if particle.mass else None
     mass = particle.mass * Unit('amu') if particle.mass else None
 
     site = Atom(
