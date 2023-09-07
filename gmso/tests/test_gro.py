552535020c773764c66f3889adb52328ef2e38cb
Co Quach <daico007@gmail.com>
2023-09-06 21:53:55 -0500

    address remaining comment

--- gmso/tests/test_gro.py
+++ gmso/tests/test_gro.py
@@ -48,12 +48,12 @@ class TestGro(BaseTest):
         top = from_parmed(pmd.load_file(get_fn("ethane.gro"), structure=True))
         top.save("out.gro")
 
-        read_top = Topology("out.gro")
-        assert np.all(list(map(lambda x: x.positions >= 0, read_top.sites)))
-
     def test_write_gro_with_shift_coord(self):
-        top = from_parmed(pmd.load_file(get_fn("ethane.gro"), structure=True))
+        top = from_parmed(pmd.load_file(get_fn("ethane.mol2"), structure=True))
         top.save("out.gro", shift_coord=True)
+
+        read_top = Topology.load("out.gro")
+        assert np.all(list(map(lambda x: x.position >= 0, read_top.sites)))
 
     def test_write_gro_non_orthogonal(self):
         top = from_parmed(pmd.load_file(get_fn("ethane.gro"), structure=True))
