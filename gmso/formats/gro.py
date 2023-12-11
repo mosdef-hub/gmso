fix bug in shifting units but not normalizing by a unit array

gmso/formats/gro.py
@@ -163,7 +163,7 @@ def _validate_positions(pos_array):
         )
     min_xyz = np.min(pos_array, axis=0)
     unit = min_xyz.units
-    min_xyz0 = np.where(min_xyz < 0 * unit, min_xyz, 0 * unit)
+    min_xyz0 = np.where(min_xyz < 0 * unit, min_xyz, 0 * unit) * unit
 
     pos_array -= min_xyz0
 