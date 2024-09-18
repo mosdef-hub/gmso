953371ca0ef301d6232eb80eb2f87c71fdb48abe
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-09-17 22:12:33 -0600

    call repr method in test

--- gmso/tests/abc/test_abc.py
+++ gmso/tests/abc/test_abc.py
@@ -7,13 +7,13 @@ from gmso.tests.base_test import BaseTest
 class TestSite(BaseTest):
     def test_molecule(self):
         molecule = Molecule()
-        molecule
+        molecule.__repr__()
         assert molecule.number == 0
         assert molecule.isrigid is False
 
     def test_residue(self):
         residue = Residue()
-        residue
+        residue.__repr__()
         assert residue.number == 0
 
     def test_site(self):
