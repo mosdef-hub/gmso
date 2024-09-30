7bfbb3f63d770bca4d73fa2a952fc26fc7be048a
CalCraven <nicholas.c.craven@vanderbilt.edu>
2024-09-30 10:00:58 -0400

    Add compatibility checks for charmm parm files

--- gmso/formats/prm_writer.py
+++ gmso/formats/prm_writer.py
@@ -1,10 +1,10 @@
 """CHARMM Par format."""
 
-from gmso.formats.formats_registry import saves_as
-from gmso.utils.units import LAMMPS_UnitSystems
-from gmso.utils.compatibility import check_compatibility
 from gmso.core.views import PotentialFilters
+from gmso.formats.formats_registry import saves_as
 from gmso.lib.potential_templates import PotentialTemplateLibrary
+from gmso.utils.compatibility import check_compatibility
+from gmso.utils.units import LAMMPS_UnitSystems
 
 
 def _validate_potential_compatibility(top):
@@ -14,6 +14,7 @@ def _validate_potential_compatibility(top):
         top, _accepted_potentials(), site_pfilter=pfilter, conn_pfilter=pfilter
     )
     return pot_types
+
 
 def _accepted_potentials():
     """List of accepted potentials that LAMMPS can support."""
@@ -28,17 +29,15 @@ def _accepted_potentials():
         lennard_jones_potential,
         harmonic_bond_potential,
         harmonic_angle_potential,
-		ub_angle_potential,
+        ub_angle_potential,
         periodic_torsion_potential,
         harmonic_improper_potential,
     ]
     return accepted_potentialsList
 
 
-
-
 @saves_as(".prm", ".par")
-def write_prm(topology, filename):
+def write_prm(topology, filename, strict_potentials=False):
     """Write CHARMM Parameter .prm file given a parametrized structure.
 
     Notes
@@ -52,9 +51,19 @@ def write_prm(topology, filename):
     Parmed stores rmin/2 in "rmin"
     """
     # Validation
+    # TODO: Use strict_x, (e.g. x=bonds) to validate what topology attrs   to convert
+    if not strict_potentials:
+        default_parameterMaps = {  # TODO: sites are not checked currently because gmso
+            # doesn't store pair potential eqn the same way as the connections.
+            "impropers": ["LAMMPSHarmonicImproperPotential"],
+            "dihedrals": ["PeriodicTorsionPotential"],
+            "angles": ["LAMMPSHarmonicAnglePotential", "UreyBradleyAnglePotential"],
+            "bonds": ["LAMMPSHarmonicBondPotential"],
+            # "sites":"LennardJonesPotential",
+            # "sites":"CoulombicPotential"
+        }
+        _try_default_potential_conversions(topology, default_parameterMaps)
     potentialsMap = _validate_potential_compatibility(topology)
-
-
 
     unit_system = LAMMPS_UnitSystems("real")  # ang, kcal/mol, amu
     # ATOMS
@@ -79,14 +88,12 @@ def write_prm(topology, filename):
             atom1, atom2 = bond.member_types
             f.write(
                 "{:8s} {:8s} {:7s} {:7s}\n".format(
-
-                    atom1, atom2,
-                    unit_system.convert_parameter(
-                        bond.parameters["k"], n_decimals=3
-                    ),
+                    atom1,
+                    atom2,
+                    unit_system.convert_parameter(bond.parameters["k"], n_decimals=3),
                     unit_system.convert_parameter(
                         bond.parameters["r_eq"], n_decimals=3
-                    )
+                    ),
                 )
             )
 
@@ -103,7 +110,9 @@ def write_prm(topology, filename):
                             angle.parameters["k"], n_decimals=3
                         ),
                         unit_system.convert_parameter(
-                            angle.parameters["theta_eq"], n_decimals=3, name="theta_eq" # necessary for conversion to degrees not radians
+                            angle.parameters["theta_eq"],
+                            n_decimals=3,
+                            name="theta_eq",  # necessary for conversion to degrees not radians
                         ),
                         unit_system.convert_parameter(
                             angle.parameters["kub"], n_decimals=3
@@ -123,8 +132,7 @@ def write_prm(topology, filename):
                             angle.parameters["k"], n_decimals=3
                         ),
                         unit_system.convert_parameter(
-                            angle.parameters["theta_eq"], n_decimals=3,
-                            name="theta_eq"
+                            angle.parameters["theta_eq"], n_decimals=3, name="theta_eq"
                         ),
                     )
                 )
@@ -143,18 +151,13 @@ def write_prm(topology, filename):
                         atom2,
                         atom3,
                         atom4,
-                        unit_system.convert_parameter(
-                            k, n_decimals=3
-                        ),
-                        unit_system.convert_parameter(
-                            n, n_decimals=3
-                        ),
+                        unit_system.convert_parameter(k, n_decimals=3),
+                        unit_system.convert_parameter(n, n_decimals=3),
                         unit_system.convert_parameter(
                             phi_eq, n_decimals=3, name="phi_eq"
                         ),
                     )
                 )
-
 
         f.write("\nIMPROPER\n")
         for improper in topology.improper_types:
@@ -200,3 +203,16 @@ def write_prm(topology, filename):
                 )
             )
         f.write("\nEND")
+
+
+def _try_default_potential_conversions(top, potentialsDict):
+    """Take a topology a convert all potentials to the style in potentialDict.""" ""
+    for pot_container in potentialsDict:
+        containerStr = pot_container[:-1] + "_types"
+        if getattr(top, containerStr):
+            for potential in potentialsDict[pot_container]:
+                top.convert_potential_styles({pot_container: potential})
+        elif getattr(top, pot_container):
+            raise AttributeError(
+                f"Missing parameters in {pot_container} for {top.get_untyped(pot_container)}"
+            )
