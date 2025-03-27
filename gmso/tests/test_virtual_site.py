78365f72c5e6adb41de2acb5a3a0cdbd71ab7be3
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2025-03-26 21:23:01 -0600

    add initial unit test for VirtualSite class

---
+++ gmso/tests/test_virtual_site.py
@@ -0,0 +1,19 @@
+import pytest
+
+import mbuild as mb
+import numpy as np
+import unyt as u
+from pydantic import ValidationError
+
+from gmso.core.virtual_site import VirtualSite 
+from gmso.core.atom import Atom
+from gmso.tests.base_test import BaseTest
+
+
+class TestVirturalSite(BaseTest):
+    def test_new_site(self, water_system):
+        v_site = VirtualSite(parent_atoms = water_system.sites)
+        water_system.add_site(v_site)
+        assert water_system.n_sites == 4
+        for site in v_site.parent_atoms:
+            assert site in water_system.sites
