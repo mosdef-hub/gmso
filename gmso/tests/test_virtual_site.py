cc5e6121a81c14a535d73ee6d917e013d93dd0e8
CalCraven <nicholas.c.craven@vanderbilt.edu>
2025-04-11 08:34:13 -0500

    Add tests for initializing virtual_type objects

--- gmso/tests/test_virtual_site.py
+++ gmso/tests/test_virtual_site.py
@@ -1,4 +1,11 @@
+import unyt as u
+
 from gmso.core.virtual_site import VirtualSite
+from gmso.core.virtual_type import (
+    VirtualPositionType,
+    VirtualPotentialType,
+    VirtualType,
+)
 from gmso.tests.base_test import BaseTest
 
 
@@ -8,3 +15,33 @@ class TestVirturalSite(BaseTest):
         assert len(v_site.parent_atoms) == 3
         for site in v_site.parent_atoms:
             assert site in water_system.sites
+
+    def test_virtual_position(self):
+        # TODO: Check position as a function of virtual_position_type
+        pass
+
+    def test_virtual_type(self):
+        v_pot = VirtualPotentialType(
+            charge=0.1,
+            expression="5*a*b",
+            independent_variables={"a"},
+            parameters={"b": 1 * u.kJ},
+        )
+        assert v_pot
+        v_pos = VirtualPositionType(
+            expression="ri+b",
+            independent_variables="ri",
+            parameters={"b": [1, 0, 0] * u.nm},
+        )
+        assert v_pos
+        v_type = VirtualType(
+            virtual_potential=v_pot, virtual_position=v_pos, member_types=("C", "C")
+        )
+        assert v_type
+
+        v_pos2 = VirtualPositionType(
+            expression="ri+b",
+            independent_variables="ri",
+            parameters={"b": [1, 0, 0] * u.nm},
+        )
+        assert v_pos2 == v_pos
