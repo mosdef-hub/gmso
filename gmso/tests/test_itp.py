6162b5feb530b8149a1fdce35ea6800d66c261a7
Arun <askforarun@gmail.com>
2025-08-07 15:57:37 +0100

    changes in testing

--- gmso/tests/test_itp.py
+++ gmso/tests/test_itp.py
@@ -6,15 +6,14 @@ from gmso.tests.utils import get_path
 class Testitp(BaseTest):
     def test_itp(self):
         # top = Topology.load(get_fn("acn.gro"))
-        top = read_itp(get_path("LIQ.itp"))
+        top = read_itp(get_path("PNB.itp"))
         assert top != None
 
-        assert len(top.atom_types) == 14
-        # assert len(set(top.atom_types))==4
-        assert len(set([atype.name for atype in top.atom_types])) == 4
+        assert len(top.atom_types) == 516
+        assert len(set([atype.name for atype in top.atom_types])) == 6
 
-        assert len(top.bond_types) == 13
-        assert top.bonds[0].bond_type.parameters["k"] == 0.15380
+        assert len(top.bond_types) == 545
+        assert top.bonds[0].bond_type.parameters["k"] == 0.1529
         empty_set = set()
         for bond in top.bonds:
             b_type_tuple = (
@@ -22,7 +21,7 @@ class Testitp(BaseTest):
                 float(bond.bond_type.parameters["r_eq"].value),
             )
             empty_set.add(b_type_tuple)
-        assert len(empty_set) == 4
+        assert len(empty_set) == 6
 
         for bond in top.bonds:
             b_type_tuple = (
@@ -30,4 +29,4 @@ class Testitp(BaseTest):
                 float(bond.bond_type.parameters["r_eq"].value),
             )
             empty_set.add(b_type_tuple)
-            assert len(empty_set) == 4
+            assert len(empty_set) == 6
