f9d04d633a0aafa7d7c664390e069824ea6024d9
Arun <askforarun@gmail.com>
2025-09-04 14:03:13 +0100

    added tests for angles and dihedrals

--- gmso/tests/test_itp.py
+++ gmso/tests/test_itp.py
@@ -19,13 +19,26 @@ class Testitp(BaseTest):
             )
             empty_set.add(b_type_tuple)
         assert len(empty_set) == 4
-        for bond in top.bonds:
-            b_type_tuple = (
-                float(bond.bond_type.parameters["k"].value),
-                float(bond.bond_type.parameters["r_eq"].value),
+
+        for angle in top.angles:
+            a_type_tuple = (
+                float(angle.angle_type.parameters["k"].value),
+                float(angle.angle_type.parameters["theta_eq"].value),
             )
-            empty_set.add(b_type_tuple)
-            assert len(empty_set) == 4
+            empty_set.add(a_type_tuple)
+        assert len(empty_set) == 10
+
+        for dihedral in top.dihedrals:
+            d_type_tuple = (
+            float(dihedral.dihedral_type.parameters["k"].value),
+            float(dihedral.dihedral_type.parameters["phi_eq"].value),
+            float(dihedral.dihedral_type.parameters["n"].value),
+        )
+            empty_set.add(d_type_tuple)
+        assert len(empty_set) == 14
+
+
+
 
     def test_itp_PNB(self):
         top = read_itp(get_path("PNB.itp"))
@@ -45,10 +58,22 @@ class Testitp(BaseTest):
             empty_set.add(b_type_tuple)
         assert len(empty_set) == 6
 
-        for bond in top.bonds:
-            b_type_tuple = (
-                float(bond.bond_type.parameters["k"].value),
-                float(bond.bond_type.parameters["r_eq"].value),
+        for angle in top.angles:
+            a_type_tuple = (
+                float(angle.angle_type.parameters["k"].value),
+                float(angle.angle_type.parameters["theta_eq"].value),
             )
-            empty_set.add(b_type_tuple)
-            assert len(empty_set) == 6
+            empty_set.add(a_type_tuple)
+        assert len(empty_set) == 16
+
+        for dihedral in top.dihedrals:
+            d_type_tuple = (
+                float(dihedral.dihedral_type.parameters["c0"].value),
+                float(dihedral.dihedral_type.parameters["c1"].value),
+                float(dihedral.dihedral_type.parameters["c2"].value),
+                float(dihedral.dihedral_type.parameters["c3"].value),
+                float(dihedral.dihedral_type.parameters["c4"].value),
+                float(dihedral.dihedral_type.parameters["c5"].value),
+            )
+            empty_set.add(d_type_tuple)
+        assert len(empty_set) == 28
