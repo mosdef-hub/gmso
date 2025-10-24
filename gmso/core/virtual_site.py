1885411675518e263d5db8002fdf5b4f6d6aaed0
CalCraven <nicholas.c.craven@vanderbilt.edu>
2025-10-24 15:00:39 -0500

    move expression evaluation for position to gmso expression.py, and add and test for different exceptions

--- gmso/core/virtual_site.py
+++ gmso/core/virtual_site.py
@@ -1,11 +1,28 @@
+import string
 from typing import Callable, List, Optional, Union
 
+import numpy as np
 import unyt as u
 from pydantic import ConfigDict, Field
+from sympy import Function, Matrix, symbols
 
 from gmso.abc.abstract_site import Site
 from gmso.core.virtual_type import VirtualType
-from gmso.exceptions import GMSOError, MissingPotentialError
+from gmso.exceptions import MissingPotentialError
+
+
+class norm(Function):
+    """Sympy functions for use in lambdify"""
+
+    @classmethod
+    def eval(cls, arg):
+        return None
+
+
+# Evaluate vector norm
+def norm_evaluation(matrix_arg):
+    """Evaluation method for norm of a sympy matrix"""
+    return np.linalg.norm(matrix_arg)
 
 
 class VirtualSite(Site):
@@ -58,8 +75,8 @@ class VirtualSite(Site):
         """Reminder that the order of sites is fixed, such that site index 1 corresponds to ri in the self.virtual_type.virtual_position expression."""
         return self.__dict__.get("parent_sites_", [])
 
-    def position(self) -> str:
-        """Not yet implemented function to get position from virtual_type.virtual_position and parent_sites."""
+    def position(self) -> u.unyt_array:
+        """On the fly position evaluation from virtual_type.virtual_position and parent_sites."""
         if not self.virtual_type:
             raise MissingPotentialError(
                 "No VirtualType associated with this VirtualSite."
@@ -68,66 +85,24 @@ class VirtualSite(Site):
             raise MissingPotentialError(
                 "No VirtualPositionType associated with this VirtualType."
             )
-        import string
 
-        import numpy as np
-        import sympy
-        from sympy import Symbol, sympify
+        independent_namespace = {}
+        for _, symbol in zip(range(len(self.parent_sites)), string.ascii_lowercase[8:]):
+            x, y, z = symbols(f"r{symbol}1 r{symbol}2 r{symbol}3")
+            independent_namespace[f"r{symbol}"] = Matrix([x, y, z])
 
-        class norm(sympy.Function):
-            @classmethod
-            def eval(cls, arg):
-                return None
-
-        # Evaluate vector norm
-        def norm_evaluation(matrix_arg):
-            return np.linalg.norm(matrix_arg)
-
-        # String expression
-        expr_string = str(self.virtual_type.virtual_position.expression)
-        namespace = {
-            sym: Symbol(sym) for sym in self.virtual_type.virtual_position.parameters
-        }
-        namespace["norm"] = norm
-        args = [
-            namespace[sym] for sym in self.virtual_type.virtual_position.parameters
-        ]  # args to lambdify
-
-        for atom, symbol in zip(
-            range(len(self.parent_sites)), string.ascii_lowercase[8:]
-        ):
-            x, y, z = sympy.symbols(f"r{symbol}1 r{symbol}2 r{symbol}3")
-            namespace[f"r{symbol}"] = sympy.Matrix([x, y, z])
-            args.extend([x, y, z])
-
-        # Parse expression
-        try:
-            expr = sympify(expr_string, locals=namespace)
-        except (ValueError, TypeError):
-            raise GMSOError(
-                f"Expression passed {expr_string=} was not viable in sympy."
-            )
-
-        f = sympy.lambdify(args, expr, modules=[{"norm": norm_evaluation}, "numpy"])
-
-        # Evaluate
-        parameters = {
-            param: val.to_value()
-            for param, val in self.virtual_type.virtual_position.parameters.items()
-        }
+        independent_parameters = {}
         for symbol, site in zip(string.ascii_lowercase[8:], self.parent_sites):
             for i, pos in enumerate(site.position):
-                parameters[f"r{symbol}{i + 1}"] = float(pos.value)
-        result = f(**parameters)
-        if isinstance(result, Symbol) or not np.issubdtype(result.dtype, np.floating):
-            raise GMSOError(
-                f"{self=} expression was not able to be fully evaluated. Unknown parameters left defined in {expr_string=}. Position evaluation is: {result=}"
-            )
-        elif any(np.isnan(result)):
-            raise GMSOError(
-                f"Failed evaluation of {self=} with {expr_string=} and {parameters=}."
-            )
-        return np.array(result).T * u.nm
+                independent_parameters[f"r{symbol}{i + 1}"] = float(pos.value)
+
+        # perform expression evaluation
+        return (
+            self.virtual_type.virtual_position.potential_expression.evaluate(
+                independent_namespace, independent_parameters
+            ).T
+            * u.nm
+        )
 
     def __repr__(self):
         return self.name + ": -".join(site.__repr__() for site in self.parent_sites)
