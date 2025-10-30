8ae09f9e26b882fa7757da4055e33ef89539e68b
CalCraven <nicholas.c.craven@vanderbilt.edu>
2025-10-30 16:10:44 -0500

    Bump to version 0.15.0

--- gmso/__init__.py
+++ gmso/__init__.py
@@ -23,7 +23,7 @@ from .core.topology import Topology
 from .core.virtual_site import VirtualSite
 from .core.virtual_type import VirtualType
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 
 class DeduplicationFilter(logging.Filter):
