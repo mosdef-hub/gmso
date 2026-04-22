Fix logic in checking for non-zero scaling factors, update unit test

gmso/external/convert_hoomd.py
@@ -1065,7 +1065,7 @@ def _parse_coulombic(
         coulombic = hoomd.md.long_range.pppm.make_pppm_coulomb_forces(
             nlist=nlist, resolution=resolution, order=order, r_cut=r_cut
         )
-    if not np.all(scaling_factors):
+    if not np.any(scaling_factors):
         return [*coulombic]  # return early
     # Handle 1-2, 1-3, and 1-4 scaling
     # TODO: Figure out a more general way to do this and handle molecule scaling factors
@@ -1116,7 +1116,7 @@ def _parse_lj(top, atypes, combining_rule, r_cut, nlist, scaling_factors):
     # Handle 1-2, 1-3, and 1-4 scaling
     # TODO: Figure out a more general way to do this
     # and handle molecule scaling factors
-    if not np.all(scaling_factors):
+    if not np.any(scaling_factors):
         return [lj]
     special_lj = hoomd.md.special_pair.LJ()
 

gmso/tests/test_hoomd.py
@@ -551,9 +551,12 @@ class TestHoomd(BaseTest):
         np.testing.assert_allclose(forces["bonds"][0].params["CT-HC"]["sigma"], 1)
         np.testing.assert_allclose(forces["bonds"][0].params["CT-HC"]["delta"], 0.1)
 
-    def test_no_special(self, typed_ethane):
-        typed_ethane.scaling_factors[0][2] = 0
-        typed_ethane.scaling_factors[1][2] = 0
+    def test_no_special(self, ethane):
+        oplsaa = ForceField("oplsaa")
+        oplsaa.scaling_factors["electrostatics14Scale"] = 0
+        oplsaa.scaling_factors["nonBonded14Scale"] = 0
+
+        typed_ethane = apply(ethane, oplsaa)
         # 1-4 lj and charge
         forces, _ = to_hoomd_forcefield(typed_ethane, r_cut=1.2)
         assert len(forces["nonbonded"]) == 3  # no special_pairs forces